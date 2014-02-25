e132f747701995394f8f4c101b1e464f1f684c56
grin <bartek@mojotech.com>
2014-02-25 17:40:55 +0100

    Initial app stub with sequelize and postgres

---
+++ app/app.js
@@ -0,0 +1,53 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var when = require('when');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+app.Models = require('./models');
+
+when(app)
+  .then(appConfig)
+  .then(app.Models.init)
+  .then(appStart)
+  .catch(function(err) {
+    console.log(err.stack);
+  });
+
+function appConfig(app) {
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  return app;
+}
+
+function appStart() {
+  app.sequelize
+    .authenticate()
+    .then(function() {
+      http.createServer(app).listen(app.get('port'), function() {
+        console.log('Bunsen server listening on port ' + app.get('port'));
+      })
+    });
+};
---
+++ app/models/index.js
@@ -0,0 +1,42 @@
+module.exports.init = function(app) {
+  app.Models = app.Models || {};
+
+  var fs = require('fs'),
+    path = require('path'),
+    Sequelize = require('sequelize'),
+    _ = require('lodash');
+
+  try {
+    config = require('../config/config.json');
+  } catch(e) {
+    throw new Error('Error reading Sequelize config from /config/config.json')
+  }
+
+  if (config[app.get('env')]) {
+    config = config[app.get('env')];
+  } else {
+    throw new Error("'" + app.get('env') + "'" + "environment not defined in /config/config.json")
+  }
+
+  var sequelize = new Sequelize(config['database'], config['user'], config['password'], {dialect: "postgres"});
+
+  fs.readdirSync(__dirname)
+    .filter(function(file) {
+      return (file.indexOf('.') !== 0) && (file !== 'index.js')
+    })
+    .forEach(function(file) {
+      var model = sequelize.import(path.join(__dirname, file));
+      app.Models[model.name] = model;
+    });
+
+  Object.keys(app.Models).forEach(function(modelName) {
+    if ('associate' in app.Models[modelName]) {
+      app.Models[modelName].associate(app.Models);
+    }
+  });
+
+  app.sequelize = sequelize;
+  app.Sequelize = Sequelize;
+
+  return app;
+};
