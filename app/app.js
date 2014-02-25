aada5316a8fa4d654cfa366ed8b59c72c40a5b8c
Sam Saccone <samccone@gmail.com>
2014-02-25 15:10:28 -0500

    allow CORS in development mode

--- app/app.js
+++ app/app.js
@@ -35,13 +35,29 @@ function appConfig(app) {
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
-  app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 
   // development only
   if ('development' == app.get('env')) {
+    // allow cross origin requests in DEV
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,HEAD');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+      // intercept OPTIONS method
+      if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
+    });
+
     app.use(express.errorHandler());
   }
+
+  app.use(app.router);
 
   return app;
 }
