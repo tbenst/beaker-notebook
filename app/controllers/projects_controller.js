b15ccf3bcef74f32320474b815d56bbf7fae2727
grin <bartek@mojotech.com>
2014-02-26 15:42:00 +0100

    Projects REST API

---
+++ app/controllers/projects_controller.js
@@ -0,0 +1,68 @@
+module.exports = function(app) {
+
+  var Project = app.Models.Project,
+      User = app.Models.User;
+
+  return {
+    userIdParam: function(req, res, next, id) {
+      User.find({where: {id: req.params.user_id}})
+        .then(function(user) {
+          if (!user) {
+            throw new Error('User not found');
+          }
+          req.user = user;
+        })
+        .done(next, next);
+    },
+
+    projectIdParam: function(req, res, next, id) {
+      req.user.getProjects({where: {id: req.params.project_id}})
+        .then(function(projects) {
+          if (!projects || !projects[0]) {
+            throw new Error('Project not found');
+          }
+          req.project = projects[0];
+        })
+        .done(next, next);
+    },
+
+    index: function(req, res, next) {
+      req.user.getProjects()
+        .then(function(projects) {
+          res.json(projects);
+        })
+        .catch(next);
+    },
+
+    create: function(req, res, next) {
+      Project.create(req.body, ['name', 'description'])
+        .then(function(project) {
+          return req.user.addProject(project);
+        })
+        .then(function(project) {
+          res.json(project);
+        })
+        .catch(next);
+    },
+
+    get: function(req, res, next) {
+      res.json(req.project);
+    },
+
+    update: function(req, res, next) {
+      req.project.updateAttributes(req.body, ['name', 'description'])
+        .then(function(project) {
+          res.json(project);
+        })
+        .catch(next);
+    },
+
+    destroy: function(req, res, next) {
+      req.project.destroy()
+        .then(function(project) {
+          res.json(project);
+        })
+        .catch(next);
+    }
+  };
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
 module.exports.init = function(app) {
+
+  // DataSets
   var DataSetsController = app.Controllers.DataSetsController;
 
   app.param('data_set_id', DataSetsController.idParam);
@@ -8,6 +10,19 @@ module.exports.init = function(app) {
   app.get('/api/data_sets/:data_set_id', DataSetsController.get);
   app.put('/api/data_sets/:data_set_id', DataSetsController.update);
   app.del('/api/data_sets/:data_set_id', DataSetsController.destroy);
+
+
+  // Projects
+  var ProjectsController = app.Controllers.ProjectsController;
+
+  app.param('user_id', ProjectsController.userIdParam);
+  app.param('project_id', ProjectsController.projectIdParam);
+
+  app.get('/api/users/:user_id/projects', ProjectsController.index);
+  app.post('/api/users/:user_id/projects', ProjectsController.create);
+  app.get('/api/users/:user_id/projects/:project_id', ProjectsController.get);
+  app.put('/api/users/:user_id/projects/:project_id', ProjectsController.update);
+  app.del('/api/users/:user_id/projects/:project_id', ProjectsController.destroy);
 
   return app;
 };
