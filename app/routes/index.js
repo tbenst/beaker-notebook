5f43c98af46b20689145a2c4f1df86b393378813
grin <bartek@mojotech.com>
2014-02-26 16:21:08 +0100

    Split routes into separate files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,28 +1,7 @@
 module.exports.init = function(app) {
 
-  // DataSets
-  var DataSetsController = app.Controllers.DataSetsController;
-
-  app.param('data_set_id', DataSetsController.idParam);
-
-  app.get('/api/data_sets', DataSetsController.index);
-  app.post('/api/data_sets', DataSetsController.create);
-  app.get('/api/data_sets/:data_set_id', DataSetsController.get);
-  app.put('/api/data_sets/:data_set_id', DataSetsController.update);
-  app.del('/api/data_sets/:data_set_id', DataSetsController.destroy);
-
-
-  // Projects
-  var ProjectsController = app.Controllers.ProjectsController;
-
-  app.param('user_id', ProjectsController.userIdParam);
-  app.param('project_id', ProjectsController.projectIdParam);
-
-  app.get('/api/users/:user_id/projects', ProjectsController.index);
-  app.post('/api/users/:user_id/projects', ProjectsController.create);
-  app.get('/api/users/:user_id/projects/:project_id', ProjectsController.get);
-  app.put('/api/users/:user_id/projects/:project_id', ProjectsController.update);
-  app.del('/api/users/:user_id/projects/:project_id', ProjectsController.destroy);
+  require('./data_sets_routes.js')(app);
+  require('./projects_routes.js')(app);
 
   return app;
 };
---
+++ app/routes/projects_routes.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+
+  var ProjectsController = app.Controllers.ProjectsController;
+
+  app.param('user_id', ProjectsController.userIdParam);
+  app.param('project_id', ProjectsController.projectIdParam);
+
+  app.get('/api/users/:user_id/projects', ProjectsController.index);
+  app.post('/api/users/:user_id/projects', ProjectsController.create);
+  app.get('/api/users/:user_id/projects/:project_id', ProjectsController.get);
+  app.put('/api/users/:user_id/projects/:project_id', ProjectsController.update);
+  app.del('/api/users/:user_id/projects/:project_id', ProjectsController.destroy);
+};
