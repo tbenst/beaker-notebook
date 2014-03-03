d27a4f088e63c33334419802d605f86a7be25f52
Sam Saccone <samccone@gmail.com>
2014-03-03 14:46:24 -0500

    replace hardcoded user ID to a shared user var

--- front_end/assets/js/controllers/_project.js
+++ front_end/assets/js/controllers/_project.js
@@ -2,14 +2,14 @@
   app.controller('project', ['$scope', 'Restangular', '$state', function($scope, Restangular, $state) {
     var R = Restangular;
 
-    R.one('users', 1)
+    R.one('users', window.userID)
       .one('projects', $state.params.id)
       .get().then(function(d) {
         $scope.project = d;
       });
 
     $scope.deleteProject = function() {
-      R.one('users', 1)
+      R.one('users', window.userID)
        .one('projects', $state.params.id)
        .remove().then(function() {
           $state.go('projects');
