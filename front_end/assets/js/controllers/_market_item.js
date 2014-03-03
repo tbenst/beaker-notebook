ee260ee04d999ad11d47e13559a940e209eb32a5
Sam Saccone <samccone@gmail.com>
2014-03-03 15:23:54 -0500

    add subscription front end

--- front_end/assets/js/controllers/_market_item.js
+++ front_end/assets/js/controllers/_market_item.js
@@ -1,7 +1,23 @@
 !(function(angular, app) {
   app.controller('marketItem', ['$scope', 'Restangular', '$state', function($scope, Restangular, $state) {
-    Restangular.one('data_sets', $state.params.id).get().then(function(d) {
+    var R = Restangular;
+    R.one('data_sets', $state.params.id).get().then(function(d) {
       $scope.item = d;
+      $scope.subscribed = !!(_.findWhere(d.users, {id: window.userID}));
     });
+
+    $scope.unsubscribe = function() {
+      R.one('users', window.userID)
+       .one('subscriptions', $state.params.id).remove().then(function(d) {
+        $scope.subscribed = false;
+       });
+    }
+
+    $scope.subscribe = function() {
+      R.one('users', window.userID)
+       .one('subscriptions', $state.params.id).put().then(function(d) {
+        $scope.subscribed = true;
+       });
+    }
   }]);
 })(angular, window.bunsen);
