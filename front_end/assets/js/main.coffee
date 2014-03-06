5139e29b2d3dbba5b6bc30ee440e8272b7559009
Sam Saccone <samccone@gmail.com>
2014-03-06 13:58:43 -0500

    split route files

--- front_end/assets/js/main.coffee
+++ front_end/assets/js/main.coffee
@@ -1,3 +1,3 @@
 #= require "_bunsen_app.js"
-#= require "_router.js"
+#= require_tree "routes"
 #= require_tree "controllers"
--- front_end/assets/js/routes/_market.js
+++ front_end/assets/js/routes/_market.js
@@ -1,35 +1,5 @@
 !(function(angular, app, templates) {
   app.config(['$stateProvider', '$urlRouterProvider', function setRoutes($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('projects', {
-      url: '/projects',
-      views: {
-        app: {
-          controller: 'projects',
-          template: templates.projects
-        }
-      }
-    });
-
-    $stateProvider.state('project', {
-      url: '/projects/:id',
-      views: {
-        app: {
-          controller: 'project',
-          template: templates.project
-        }
-      }
-    });
-
-    $stateProvider.state('projectEdit', {
-      url: '/projects/:id/edits',
-      views: {
-        app: {
-          controller: 'projectEdit',
-          template: templates.project_edit
-        }
-      }
-    });
-
     $stateProvider.state('marketPlace', {
       url: '/market_place',
       views: {
@@ -50,5 +20,4 @@
       }
     });
   }]);
-
 })(angular, window.bunsen, templates);
