623d063f30651fca5ad114287e26714f33c81746
Sam Saccone <samccone@gmail.com>
2014-03-07 16:46:03 -0500

    add tree view controler and custom tag filter
    Conflicts:
    	front_end/assets/templates/market_place.jade

--- front_end/assets/js/controllers/_market_place.js
+++ front_end/assets/js/controllers/_market_place.js
@@ -3,5 +3,32 @@
     Restangular.one('data_sets').getList().then(function(d) {
       $scope.data = d;
     });
+
+    $scope.treeOptions = {
+      nodeChildren: "children",
+      dirSelectable: true,
+    }
+
+    $scope.onTreeSelection = function(node) {
+      $scope.categoryFilterBy = node.category;
+    }
+
+    $scope.categoryFilter = function() {
+      return true;
+    }
+
+    $scope.treeData = [
+      { "category" : "hats", "count" : "2", "children" : [
+        {
+          "category": "top hats", "count" : 1, children: []
+        },
+        {
+          "category": "baseball hats", "count" : 1, children: []
+        },
+      ]},
+      { "category" : "pants", "count": 4, children: []},
+      { "category" : "shirts", "count": 4, children: []},
+    ];
+
   }]);
 })(angular, window.bunsen);
--- front_end/assets/js/main.coffee
+++ front_end/assets/js/main.coffee
@@ -1,3 +1,4 @@
 #= require "_bunsen_app.js"
 #= require_tree "routes"
 #= require_tree "controllers"
+#= require_tree "filters"
