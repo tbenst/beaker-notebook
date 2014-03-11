2c232eb959b6aa9151152785ad17ad08ce9bf4e5
grin <bartek@mojotech.com>
2014-03-11 21:21:25 +0100

    Logic for finding data sets that belong to a category
    See: http://www.postgresql.org/docs/9.1/static/ltree.html

--- app/models/data_set.js
+++ app/models/data_set.js
@@ -1,3 +1,6 @@
+var _ = require('lodash'),
+    util = require('util');
+
 module.exports = function(sequelize, DataTypes) {
   var DataSet = sequelize.define('DataSet', {
     title: DataTypes.STRING,
@@ -12,6 +15,8 @@ module.exports = function(sequelize, DataTypes) {
     tableName: 'data_sets',
     classMethods: {
       associate: function(models) {
+        DataSet.models = models;
+
         DataSet.hasOne(models.Vendor, {
           foreignKey: 'vendor_id'
         });
@@ -29,6 +34,30 @@ module.exports = function(sequelize, DataTypes) {
           as: 'categories',
           through: 'DataSetsCategories'
         });
+      },
+
+      findByCategory: function(category, options){
+        if (!category) {return DataSet.findAll(options);}
+
+        var Category = this.models.Category;
+
+        return Category.findAll({where: ["path <@ ?", category.path]}) // ltree expression to find all categories at the level of current category or below
+          .then(function(categories) {
+            var ids = _.map(categories, function(category) {
+              return category.id;
+            }).join(",");
+
+            var query =
+              "SELECT data_sets.*\n" +
+              "FROM data_sets\n" +
+              "INNER JOIN \"DataSetsCategories\" ON data_sets.id = \"DataSetsCategories\".\"data_setId\" AND\n" +
+              "\"DataSetsCategories\".\"CategoryId\" IN (%s)\n" +
+              "LIMIT %d OFFSET %d";
+
+            query = util.format(query, ids, options['limit'], options['offset']);
+
+            return sequelize.query(query, DataSet);
+          });
       }
     }
   });
