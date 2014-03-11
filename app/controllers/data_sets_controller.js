1637c7d916bcf5374e7183ce6a62313a2f38c0ae
grin <bartek@mojotech.com>
2014-03-11 21:02:36 +0100

    Fix data set param middleware

--- app/controllers/data_sets_controller.js
+++ app/controllers/data_sets_controller.js
@@ -7,11 +7,10 @@ module.exports = function(app) {
     idParam: function(req, res, next, id) {
       DataSet.find({where: {id: req.params.data_set_id}})
         .then(function(dataSet) {
-          if (!dataSet) return next(new Error('DataSet not found'));
+          if (!dataSet) throw new Error('DataSet not found');
           req.dataSet = dataSet;
-          next();
         })
-        .catch(next);
+        .done(next, next);
     },
 
     index: function(req, res, next) {
