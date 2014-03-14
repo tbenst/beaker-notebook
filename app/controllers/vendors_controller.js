838416b67db00d3b7881c6d81c6c5695929f2b38
Sam Saccone <samccone@gmail.com>
2014-03-14 09:36:43 -0400

    add vendor api routes

---
+++ app/controllers/vendors_controller.js
@@ -0,0 +1,26 @@
+module.exports = function(app) {
+  var Vendor  = app.Models.Vendor;
+
+  return {
+    idParam: function(req, res, next) {
+      Vendor.find({where: {id: req.params.vendor_id}})
+        .then(function(vendor) {
+          if (!vendor) {
+            throw new Error('vendor not found');
+          }
+          req.vendor = vendor;
+        })
+        .done(next, next);
+    },
+
+    get: function(req, res) {
+      res.json(req.vendor);
+    },
+
+    index: function(req, res, next) {
+      Vendor.findAll().then(function(vendors) {
+        res.json(vendors);
+      }).catch(next);
+    }
+  }
+}
