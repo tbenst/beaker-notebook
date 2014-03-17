33dca457cf2ce64a778fc01be6e89d8781d245bf
Sam Saccone <samccone@gmail.com>
2014-03-17 13:37:04 -0400

    split seed fixtures up

---
+++ app/seed_files/categories.js
@@ -0,0 +1,38 @@
+module.exports =  [
+  {
+    model: 'Category',
+    data: {
+      name: "Organizations",
+      path: "1"
+    }
+  },
+  {
+    model: 'Category',
+    data: {
+      name: "U.S. Energy Information Administration",
+      path: "1.1"
+    }
+  },
+
+  {
+    model: 'Category',
+    data: {
+      name: "Consumer Finance",
+      path: "1.2"
+    }
+  },
+  {
+    model: 'Category',
+    data: {
+      name: "Currated Collections",
+      path: "2"
+    }
+  },
+  {
+    model: 'Category',
+    data: {
+      name: "United States",
+      path: "2.1"
+    }
+  }
+];
