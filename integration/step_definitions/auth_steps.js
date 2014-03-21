d62e6db4ae07bad72c85e097f2ca236cc643e888
Matthew Forsyth <matt@mojotech.com>
2014-03-21 09:22:19 -0400

    [tests] [cleanup] Use native bind rather than lodash bind.

--- integration/step_definitions/auth_steps.js
+++ integration/step_definitions/auth_steps.js
@@ -1,10 +1,9 @@
 module.exports = function() {
 
   this.Given(/^I'm signed in as a researcher$/, function() {
-    this.seed('User', userData).then(_.bind(function() {
-    }, this));
     var userData = {name: 'joe research', email: 'u@r.edu', id: 1};
-      this.driver.get(this.route.home);
+    this.seed('User', userData).then(
+      this.driver.get.bind(this.driver, this.route.home));
   });
 
 };
