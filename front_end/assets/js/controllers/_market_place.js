a9589736db158d7bf168fa7b99dca0a5bc1f13b7
Sam Saccone <samccone@gmail.com>
2014-03-21 14:00:41 -0400

    show current filter tag list on market page

--- front_end/assets/js/controllers/_market_place.js
+++ front_end/assets/js/controllers/_market_place.js
@@ -1,12 +1,31 @@
 !(function(angular, app) {
 
-  app.controller('marketPlace', ['$scope', 'Restangular', 'VendorsFactory', 'DataSetsFactory', 'RelatedTagsFactory', function($scope, Restangular, VendorsFactory, DataSetsFactory, RelatedTagsFactory) {
+  app.controller('marketPlace', ['$scope', 'Restangular', 'VendorsFactory', 'DataSetsFactory', 'RelatedTagsFactory', 'TagNormalizeService', function($scope, Restangular, VendorsFactory, DataSetsFactory, RelatedTagsFactory, TagNormalizeService) {
     $scope.isSelected = function(value, model){
       if (model === void(0)) {
         return false;
       }
 
       return ~model.indexOf(value+"");
+    }
+
+    $scope.removeFilter = function(value, model) {
+      _.remove($scope.$eval(model), function(v) {
+        return v === value;
+      });
+
+      getDataSets();
+    }
+
+    function getSelectedFilters() {
+      var TS = TagNormalizeService;
+
+      // condense all search filters into a flat list
+      return Array.prototype.concat(
+        TS.normalizeTypes($scope.marketPlace.typeScope, 'marketPlace.typeScope', $scope),
+        TS.normalizeVendors($scope.marketPlace.vendorScope, 'marketPlace.vendorScope', $scope),
+        TS.normalizeTags($scope.marketPlace.tagScope, 'marketPlace.tagScope', $scope)
+      );
     }
 
     function getDataSets() {
@@ -17,6 +36,8 @@
       RelatedTagsFactory.getItems($scope.marketPlace).then(function(tags) {
         $scope.marketPlace.relatedTags = tags;
       });
+
+      $scope.currentFilters = getSelectedFilters();
     }
 
     Restangular.one('data_tags').getList().then(function(d) {
---
+++ front_end/assets/js/services/_tag_normalize_service.js
@@ -0,0 +1,33 @@
+;(function(app) {
+  app.service('TagNormalizeService', function() {
+    return {
+      normalizeTypes: function(types, modelName, $scope) {
+        return _.map(types, function(v) {
+          return {
+            "name": v,
+            "value": v,
+            "model": modelName
+          }
+        });
+      },
+      normalizeVendors: function(vendors, modelName, $scope) {
+        return _.map(vendors, function(vendorID) {
+          return {
+            "name": _.findWhere($scope.marketPlace.vendors, {id: +vendorID}).name,
+            "value": vendorID,
+            "model": modelName
+          }
+        });
+      },
+      normalizeTags: function(tags, modelName, $scope) {
+        return _.map(tags, function(tagID) {
+          return {
+            "name": _.findWhere($scope.tags, {id: +tagID}).name,
+            "value": tagID,
+            "model": modelName
+          }
+        });
+      }
+    }
+  });
+})(window.bunsen);
