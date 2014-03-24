eda7402ab6cc81c5b38622d0900bc4db3f55842b
grin <bartek@mojotech.com>
2014-03-24 20:03:28 +0100

    Fix pagination in the data set finder sql

--- app/models/data_set.js
+++ app/models/data_set.js
@@ -51,8 +51,8 @@ module.exports = function(sequelize, DataTypes) {
       findAllSql: function() {
         var query =
           "SELECT \"DataSets\".*\n" +
-          "FROM \"DataSets\"\n" +
-          "LIMIT :limit OFFSET :offset\n";
+          "FROM \"DataSets\"";
+
         return query;
       },
 
@@ -67,9 +67,15 @@ module.exports = function(sequelize, DataTypes) {
       },
 
       findMatching: function(filters, options) {
-        return this.findMatchingSql(filters, options).then(function(query) {
-          return sequelize.query(query, DataSet, {}, options);
-        });
+        return this.findMatchingSql(filters).then(function(query) {
+          var paginatedQuery =
+            "SELECT * FROM (\n" +
+              query +
+            ") AS matching\n" +
+            "LIMIT :limit OFFSET :offset";
+
+          return sequelize.query(paginatedQuery, DataSet, {}, options);
+        })
       },
 
       findMatchingTags: function(filters) {
