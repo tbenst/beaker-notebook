e4856f46aae1ec6721f927b03416e89920c4fe4b
Sam Saccone <samccone@gmail.com>
2014-04-01 11:22:09 -0400

    Re-implement projects to use bookshelf

--- app/controllers/projects_controller.js
+++ app/controllers/projects_controller.js
@@ -1,38 +1,46 @@
-module.exports = function(app) {
+var _ = require("lodash")
 
+module.exports = function(app) {
   var Project = app.Models.Project,
       User = app.Models.User;
 
   return {
     projectIdParam: function(req, res, next, id) {
-      req.user.getProjects({where: {id: req.params.project_id}})
-        .then(function(projects) {
-          if (!projects || !projects[0]) {
-            throw new Error('Project not found');
-          }
-          req.project = projects[0];
-        })
-        .done(next, next);
+      req.user.projects()
+      .query()
+      .where("id", "=", req.params.project_id)
+      .then(function(projects) {
+        if (!projects || !projects[0]) {
+          throw new Error('Project not found');
+        }
+        req.project = Project.forge(projects[0]);
+      })
+      .done(next, next);
     },
 
     index: function(req, res, next) {
       Project.findMatching({
         userId: req.user.id,
         filterBy: decodeURIComponent(req.query.filterBy || '')
-      }).then(function(projects) {
-        res.json(projects);
-      }).catch(next);
+      })
+      .then(function(projects) {
+        res.json(projects.rows);
+      })
+      .catch(next);
     },
 
     create: function(req, res, next) {
-      Project.create(req.body, ['name', 'description'])
-        .then(function(project) {
-          return req.user.addProject(project);
-        })
-        .then(function(project) {
-          res.json(project);
-        })
-        .catch(next);
+      Project.forge(
+        _.extend({},
+          _.pick(req.body, "name", "description"),
+          {"ownerId": req.user.get('id')}
+        )
+      )
+      .save()
+      .then(function(project) {
+        res.json(project);
+      })
+      .catch(next);
     },
 
     get: function(req, res, next) {
@@ -40,19 +48,21 @@ module.exports = function(app) {
     },
 
     update: function(req, res, next) {
-      req.project.updateAttributes(req.body, ['name', 'description'])
-        .then(function(project) {
-          res.json(project);
-        })
-        .catch(next);
+      req.project.save(_.pick(req.body,
+        'name', 'description'
+      ), {patch: true})
+      .then(function(project) {
+        res.json(project);
+      })
+      .catch(next);
     },
 
     destroy: function(req, res, next) {
       req.project.destroy()
-        .then(function(project) {
-          res.json(project);
-        })
-        .catch(next);
+      .then(function(project) {
+        res.json(project);
+      })
+      .catch(next);
     }
   };
 };
