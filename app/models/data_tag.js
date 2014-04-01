eec965a5cfc622ba7056c593aff7f7ad4f6b031a
Sam Saccone <samccone@gmail.com>
2014-04-01 10:12:05 -0400

    Re-implement data tags to use bookshelf

--- app/models/data_tag.js
+++ app/models/data_tag.js
@@ -1,17 +1,21 @@
-module.exports = function(sequelize, DataTypes) {
-  var DataTag = sequelize.define('DataTag', {
-    name: DataTypes.STRING,
-  }, {
-    tableName: 'DataTags',
-    classMethods: {
-      associate: function(models) {
-        DataTag.hasMany(models.DataSet, {
-          through: "DataSetsDataTags",
-          foreignKey: "dataTagId"
-        });
-      }
+var _     = require('lodash');
+
+module.exports = function(Bookshelf, app) {
+  var query   = Bookshelf.knex;
+  var DataTag = Bookshelf.Model.extend({
+    tableName: 'DataTags'
+  });
+
+  DataTag = _.extend(DataTag, {
+    findAll: function() {
+      return DataTag.collection()
+      .query()
+      .orderBy('name', 'ASC')
     }
   });
 
-  return DataTag;
+  return {
+    name: "DataTag",
+    model: DataTag
+  };
 };
