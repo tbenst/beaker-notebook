c2e95fa331b923961efe436f312fc65b0a048cfb
grin <bartek@mojotech.com>
2014-04-01 11:41:23 +0200

    Re-implement category model

--- app/models/category.js
+++ app/models/category.js
@@ -1,80 +1,78 @@
 var _ = require('lodash'),
     util = require('util');
 
-module.exports = function(sequelize, DataTypes) {
-  var Category = sequelize.define('Category', {
-    name: DataTypes.TEXT,
-    path: DataTypes.TEXT // this has 'ltree' type in the db, but is declared as TEXT to have getters/setters
+module.exports = function(Bookshelf, app){
+  var models = app.Models;
+  var query = Bookshelf.knex;
+
+  var Category = Bookshelf.Model.extend({
+    tableName: "Categories",
+
+    dataSets: function() {
+      return this.belongsToMany(models.DataSet, 'DataSetsCategories', 'categoryId', 'dataSetId')
+    }
   }, {
-    classMethods: {
-      associate: function(models) {
-        Category.hasMany(models.DataSet, {
-          as: 'dataSets',
-          through: 'DataSetsCategories',
-          foreignKey: 'categoryId'
-        });
-      },
 
-      findAllWithCounts: function() {
-        var query =
-          "SELECT id, name, path, COUNT(\"DataSetsCategories\".\"dataSetId\") AS \"dataCount\"\n" +
-          "FROM \"Categories\"\n" +
-          "LEFT OUTER JOIN \"DataSetsCategories\" ON \"Categories\".id = \"DataSetsCategories\".\"categoryId\"\n" +
-          "GROUP BY id, name, path\n" +
-          "ORDER BY path";
+    findAllWithCounts: function() {
+      return query('Categories')
+        .select('Categories.id', 'name', 'path')
+        .count('DataSetsCategories.dataSetId AS dataCount')
+        .join('DataSetsCategories', 'Categories.id', '=', 'DataSetsCategories.categoryId', 'LEFT OUTER')
+        .groupBy('Categories.id', 'name', 'path')
+        .orderBy('path')
+    },
 
-        return sequelize.query(query);
-      },
+    tree: function() {
+      var nodes = {},
+          roots = [];
 
-      tree: function() {
-        var nodes = {},
-            roots = [];
-
-        function parent(path) {
-          var items = path.split('.');
-          if (items.length == 1) {
-            return null;
-          } else {
-            items.pop();
-            return nodes[items.join('.')];
-          }
+      function parent(path) {
+        var items = path.split('.');
+        if (items.length == 1) {
+          return null;
+        } else {
+          items.pop();
+          return nodes[items.join('.')];
         }
-
-        function initNodes(categories) {
-          _.each(categories, function(category) {
-            nodes[category.path] = {
-              category: category.name,
-              id: category.id,
-              path: category.path,
-              count: +category.dataCount,
-              children: []
-            };
-          });
-        }
-
-        return Category.findAllWithCounts()
-          .then(function(categories) {
-
-            initNodes(categories);
-
-            _.each(categories.reverse(), function(category) {
-              var parentNode = parent(category.path),
-                  node = nodes[category.path];
-              if (parentNode) {
-                parentNode.children.unshift(node);
-                parentNode.count = parentNode.count + node.count;
-              } else {
-                roots.unshift(node);
-              }
-            });
-
-            return _.map(roots, function(root) {
-              return nodes[root.path];
-            });
-          });
       }
+
+      function initNodes(categories) {
+        _.each(categories, function(category) {
+          nodes[category.path] = {
+            category: category.name,
+            id: category.id,
+            path: category.path,
+            count: +category.dataCount,
+            children: []
+          };
+        });
+      }
+
+      return Category.findAllWithCounts()
+        .then(function(categories) {
+
+          initNodes(categories);
+
+          _.each(categories.reverse(), function(category) {
+            var parentNode = parent(category.path),
+                node = nodes[category.path];
+            if (parentNode) {
+              parentNode.children.unshift(node);
+              parentNode.count = parentNode.count + node.count;
+            } else {
+              roots.unshift(node);
+            }
+          });
+
+          return _.map(roots, function(root) {
+            return nodes[root.path];
+          });
+        });
     }
   });
 
-  return Category;
-};
+  return {
+    model: Category,
+    name: "Category"
+  }
+}
