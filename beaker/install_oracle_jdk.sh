9f59745b54e17e0c5de72f0add414c6e1412a508
Matthew Forsyth <matt@mojotech.com>
2014-04-18 08:57:25 -0400

    [beaker] Install Beaker on a docker image.

---
+++ beaker/install_oracle_jdk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+apt-get install -y -qq --no-install-recommends oracle-java7-installer oracle-java7-set-default
