d012a81afb790e93209f8fdc5d38587a220a9218
grin <bartek@mojotech.com>
2014-04-30 14:59:59 +0200

    Allow researchers to import notebooks via project UI

--- front_end/assets/js/controllers/_project.js
+++ front_end/assets/js/controllers/_project.js
@@ -1,7 +1,8 @@
 !(function(angular, app) {
-  app.controller('project', ['$scope', '$state', 'Factories', '$localStorage', function($scope, $state, Factories, $localStorage) {
+  app.controller('project', ['$scope', '$state', 'Factories', '$localStorage', '$upload', 'Restangular', '$sessionStorage', function($scope, $state, Factories, $localStorage, $upload, Restangular, $sessionStorage) {
     var F = Factories;
     $scope.editMode = false;
+    $scope.importMode = false;
 
     // Save last visited project
     $localStorage.projects = $localStorage.projects || {};
@@ -24,6 +25,10 @@
 
     $scope.editProject = function() {
       $scope.editMode = true;
+    };
+
+    $scope.importNotebooks = function() {
+      $scope.importMode = true;
     };
 
     $scope.updateProject = function() {
@@ -49,6 +54,20 @@
         delete $localStorage.projects.last
         $state.go('projects.items');
       });
+    };
+
+    $scope.onFileSelect = function($files) {
+      _.each($files, function(file) {
+        var url = Restangular.one('projects', $scope.project.id).all('notebooks').one('import').getRestangularUrl();
+        $scope.upload = $upload.upload({
+          url: url,
+          method: 'POST',
+          headers: {'Authorization': $sessionStorage.currentUser.token},
+          file: file
+        }).then(function() {
+          $scope.importMode = false;
+        });
+      })
     };
   }]);
 })(angular, window.bunsen);
