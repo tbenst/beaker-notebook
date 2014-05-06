ac30e08553843fd1e8cc9b40773b2e5f9e8900f9
Sam Saccone <samccone@gmail.com>
2014-05-06 11:48:49 -0400

    Enable API root path to be defined via ENV

--- front_end/assets/js/_bunsen_app.js
+++ front_end/assets/js/_bunsen_app.js
@@ -1,6 +1,7 @@
 !(function(angular) {
   window.bunsen = angular.module('bunsen', ['ui.router', 'ui.bootstrap', 'restangular', 'treeControl', 'marketPlaceFilters', 'ngStorage', 'beakerNotebook', 'angularFileUpload'], ['RestangularProvider', function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('http://' + window.location.hostname + ':3000/api')
+    var baseUrl = window.API_BASE_URL || 'http://' + window.location.hostname + ':3000';
+    RestangularProvider.setBaseUrl(baseUrl+'/api')
   }]);
 })(angular);
 
--- front_end/readme.md
+++ front_end/readme.md
@@ -1,6 +1,11 @@
 bunsen front end
 ============
 
+## Optional Compile ENV flags
+  * `API_BASE_URL`, defaults to http://window.location.hostname:3000
+  * `NODE_ENV`, defaults to undefined
+    * If you set this to `test` jquery will be included as a script.
+
 ## Dev
   * `npm install`
   * `npm run-script bower`
