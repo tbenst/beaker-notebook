0a35aa7c39b5e26224ebd0171a9fc05ee0116182
Matthew Forsyth <matt@mojotech.com>
2014-06-03 11:20:06 -0400

    app:  respond with 500 error if seed fails.

--- app/routes/seed_routes.js
+++ app/routes/seed_routes.js
@@ -5,33 +5,32 @@ var path        = require("path");
 var _           = require("lodash");
 
 module.exports = function(app) {
-  app.post("/seed/data", function(req, res) {
+  app.post("/seed/data", function(req, res, next) {
     Seed([req.body]).then(function() {
       res.send(200);
-    });
+    })
+    .catch(next);
   });
 
-  app.post("/seed/drop-repos", function(req, res) {
+  app.post("/seed/drop-repos", function(req, res, next) {
     exec("rm -rf " + path.resolve(__dirname, '/repos'))
     .then(function() {
       res.send(200);
     })
-    .catch(function() {
-      res.send(500);
-    });
+    .catch(next);
   });
 
-  app.post("/seed/drop-all", function(req, res) {
+  app.post("/seed/drop-all", function(req, res, next) {
     Seed.dropAll().then(function() {
       res.send(200);
-    });
+    })
+    .catch(next);
   });
 
-  app.post("/seed/fetch", function(req, res) {
+  app.post("/seed/fetch", function(req, res, next) {
     app.Models[req.body.modelName].forge(req.body.data).fetch().then(function(d) {
       res.json(d);
-    }).catch(function(e) {
-      res.send(500);
-    });
+    })
+    .catch(next);
   });
 }
