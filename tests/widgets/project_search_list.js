6614390b67f66c8369db51dff3a9936124ced46d
Ryan Coughlin <ryan@ryancoughlin.com>
2014-06-06 14:52:05 -0400

    Update tests to match new search results DOM

--- tests/widgets/project_search_list.js
+++ tests/widgets/project_search_list.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return this.Widgets.ProjectSearchList = this.Widget.List.extend({
     root: '.project-search',
-    itemSelector: "li",
+    itemSelector: ".search-result-row-item",
 
     waitForItem: function() {
       return this.find(this.itemSelector);
@@ -18,7 +18,7 @@ module.exports = function() {
 
     contents: function() {
       return $.map(this.items(), function(n) {
-        return $.all([n.find(".name"), n.find(".notebooks .count")])
+        return $.all([n.find(".name a"), n.find(".vendor-item-list-attributes .count")])
         .then(function(arr) {
           return $.all(_.invoke(arr, 'getText'))
           .then(function(text) {
