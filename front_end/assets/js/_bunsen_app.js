7c3c503912c5b8a961a1ccec33a8e315839390d2
Matthew Forsyth <matt@mojotech.com>
2014-06-09 09:55:26 -0400

    front_end: Load other bunsen service urls at launch time

--- front_end/assets/js/_bunsen_app.js
+++ front_end/assets/js/_bunsen_app.js
@@ -1,6 +1,5 @@
 !(function(angular) {
   window.bunsen = angular.module('bunsen', ['ui.router', 'ui.bootstrap', 'restangular', 'treeControl', 'marketPlaceFilters', 'ngStorage', 'beakerNotebook', 'angularFileUpload', 'showOnHoverParent', 'stopEvent', 'filterMarketPlace'], ['RestangularProvider', function(RestangularProvider) {
-    RestangularProvider.setBaseUrl('http://' + window.location.hostname + ':' +
-      (window.API_PORT || 3000) + '/api');
+    RestangularProvider.setBaseUrl(window.BUNSEN_SERVICES.api);
   }]);
 })(angular);
--- front_end/main.sh
+++ front_end/main.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -e
 
+api_url="/api"
+beaker_url="/beaker"
+
 for i in "$@"
 do
 case $i in
   -w|--watch) watch=1 ;;
+  -t|--test) test=1 ;;
+  --api_url=*) api_url="${i#--api_url=}" ;;
+  --beaker_url=*) beaker_url="${i#--beaker_url=}" ;;
   -h|--help)
     cat <<EOF
 
   Usage: web [options]
   Options:
-          -h  --help      Display this message
-          -w  --watch     Restart server if files change
+          -h  --help          Display this message
+          -w  --watch         Restart server if files change
+          -t  --test          Run test server
+          --api_url=(url)     Set api URL
+          --beaker_url=(url)  Set beaker URL
 
 EOF
     exit
@@ -18,8 +27,14 @@ EOF
 esac
 done
 
+echo "window.BUNSEN_SERVICES = {api: '$api_url', beaker: '$beaker_url'};" > public/js/config.js
+
 if [[ $watch -eq 1 ]]; then
   PORT=8080 exec roots watch
+elif [[ $test -eq 1 ]]; then
+  PORT=8080 NODE_ENV=test exec roots watch
+elif [[ $shell -eq 1 ]]; then
+  exec /bin/bash
 else
   exec /usr/sbin/nginx
 fi
