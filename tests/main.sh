73311f6f06f6f73a996fccc2d805065ff80c5014
Matthew Forsyth <matt@mojotech.com>
2014-06-09 09:52:20 -0400

    tests: Create docker container for test runs.

---
+++ tests/main.sh
@@ -0,0 +1,10 @@
+# export display so firefox knows where to show
+# since we are in headless mode
+export DISPLAY=:99.0
+
+# start xdisplay
+Xvfb :99 -shmem -screen 0 1366x768x16 &
+
+# run the test
+java -jar selenium.jar &
+npm start
