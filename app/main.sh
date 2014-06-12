7bb8544e085184c42e68b2742f73c6564ec5cede
Matthew Forsyth <matt@mojotech.com>
2014-06-12 09:13:29 -0400

    app:  Expose a way to create a migration in fig env.

--- app/main.sh
+++ app/main.sh
@@ -6,6 +6,7 @@ case $i in
   -m|--migrate) migrate=1 ;;
   -s|--seed) seed=1 ;;
   -w|--watch) watch=1 ;;
+  --new-migration=*) new_migration="${i#--new-migration=}" ;;
   --delay=*) delay="${i#--delay=}" ;;
   --shell) shell=1 ;;
   -h|--help)
@@ -13,12 +14,13 @@ case $i in
 
   Usage: app [options]
   Options:
-          -h  --help      Display this message
-          -m  --migrate   Run migrations before starting app
-          -s  --seed      Seed database with fake data starting app
-          -w  --watch     Restart server if files change
-          --delay=(secs)  Delay start x seconds
-          --shell         Start interactive shell
+          -h  --help             Display this message
+          --new-migration=(name) Create a new migration
+          -m  --migrate          Run migrations before starting app
+          -s  --seed             Seed database with fake data starting app
+          -w  --watch            Restart server if files change
+          --delay=(secs)         Delay start x seconds
+          --shell                Start interactive shell
 
 EOF
     exit
@@ -32,6 +34,7 @@ color()(set -o pipefail;"$@" 2>&1>&3|sed $'s,.*,\e[31m&\e[m,'>&2)3>&1
 cd /var/app
 
 [[ -z $delay ]] || sleep $delay
+[[ -z $new_migration ]] || knex migrate:make $new_migration -v --environment=${NODE_ENV-development}
 [[ $migrate -eq 1 ]] && knex migrate:latest -v --environment=${NODE_ENV-development}
 [[ $seed -eq 1 ]] && node app_seed.js
 
