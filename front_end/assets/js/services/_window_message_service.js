e3a9cc45521344c4a47b0e20098d524e0242d761
Matthew Forsyth <matt@mojotech.com>
2014-06-16 16:54:20 -0400

    frontend: refactor: Use angular broadcast instead of manual listeners.

--- front_end/assets/js/services/_window_message_service.js
+++ front_end/assets/js/services/_window_message_service.js
@@ -1,8 +1,6 @@
 ;(function(app) {
-  app.service('WindowMessageService', ['$window', '$location', 'Factories', function($window, $location, Factories) {
-
+  app.service('WindowMessageService', ['$window', '$location', '$rootScope', 'Factories', function($window, $location, $rootScope, Factories) {
     var F = Factories;
-    var saveNotebookCallbacks = {};
 
     function saveNotebook(data) {
       var createNotebook = _.partial(F.Notebooks.createNotebook, data.projectId);
@@ -10,8 +8,7 @@
         createNotebook : F.Notebooks.updateNotebook;
       saveFunction(data.notebook).
         then(function(notebook) {
-          var callbacks = saveNotebookCallbacks[data.projectId] || []
-          _.each(callbacks, function(cb) { cb(notebook); });
+          $rootScope.$broadcast('window-message-notebook-'+data.operation, notebook);
         }, function(response) {
           $window.alert("Error attempting to " + data.operation + " notebook.");
         });
@@ -27,11 +24,7 @@
 
     $window.addEventListener('message', receiveWindowMessage, false);
 
-    return {
-      addNotebookCallback: function(projectId, cb) {
-        saveNotebookCallbacks[projectId] = saveNotebookCallbacks[projectId] || [];
-        saveNotebookCallbacks[projectId].push(cb);
-      }
-    };
+    // no public API
+    return {};
   }]);
 })(window.bunsen);
