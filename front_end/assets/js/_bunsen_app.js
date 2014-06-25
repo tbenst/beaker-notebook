34d5349f4f9381d735821e84c7fc9f8e48e7fb97
Jake Buob <jake@mojotech.com>
2014-06-25 14:33:34 -0400

    Add angular-truncate filter
    https://github.com/sparkalow/angular-truncate

--- front_end/assets/js/_bunsen_app.js
+++ front_end/assets/js/_bunsen_app.js
@@ -1,5 +1,5 @@
 !(function(angular) {
-  window.bunsen = angular.module('bunsen', ['ui.router', 'ui.bootstrap', 'restangular', 'treeControl', 'marketPlaceFilters', 'ngStorage', 'beakerNotebook', 'angularFileUpload', 'stopEvent', 'filterMarketPlace', 'hiddenUpload'], ['RestangularProvider', function(RestangularProvider) {
+  window.bunsen = angular.module('bunsen', ['ui.router', 'ui.bootstrap', 'restangular', 'treeControl', 'marketPlaceFilters', 'truncate', 'ngStorage', 'beakerNotebook', 'angularFileUpload', 'stopEvent', 'filterMarketPlace', 'hiddenUpload'], ['RestangularProvider', function(RestangularProvider) {
     RestangularProvider.setBaseUrl(window.BUNSEN_SERVICES.api);
   }]);
 })(angular);
