95fe6df83f02416c96440d893ef08ebaeff84963
Jake Buob <jake@mojotech.com>
2014-07-16 17:36:39 -0400

    frontend: Add BeakerUrlGeneratorService

--- front_end/assets/js/controllers/_notebook.js
+++ front_end/assets/js/controllers/_notebook.js
@@ -1,33 +1,19 @@
 !(function(angular, app) {
-  app.controller('notebook', ['$scope', '$state', '$sce','Factories', 'UrlGeneratorService', 'Restangular', '$sessionStorage', '$location', 'Notebooks', 'WindowMessageService', function($scope, $state, $sce, Factories, UrlGeneratorService, Restangular, $sessionStorage, $location, Notebooks, WindowMessageService) {
+  app.controller('notebook', ['$scope', '$state', '$sce', 'Factories', 'BeakerUrlGeneratorService', 'Restangular', '$sessionStorage', '$location', 'Notebooks', 'WindowMessageService', function($scope, $state, $sce, Factories, BeakerUrlGeneratorService, Restangular, $sessionStorage, $location, Notebooks, WindowMessageService) {
     var F = Factories;
-    var frame;
-    var uiUrl = $location.absUrl().split("#")[0];
-    var prjId = $state.params.id;
 
     $scope.projects.search = '';
 
-    var beakerUrl = function(subPath, params) {
-      return window.BUNSEN_SERVICES.beaker + "#/" +
-        subPath + "?" + UrlGeneratorService.toParams(_.extend(params,
-          {bunsenUiUrl: uiUrl}));
-    };
-
-    var notebookLocation = function(userId, projectId, notebookId) {
-      var bunsenUri = Restangular.one('notebooks', notebookId).one('contents').getRestangularUrl();
-
-      return beakerUrl("open", {
-        uri: bunsenUri,
-        userId: userId,
-        projectId: projectId,
-        notebookId: notebookId
-      });
-    };
-
     F.Notebooks.getNotebook($state.params.notebook_id).then(function(notebook) {
-      var userId    = $sessionStorage.currentUser.id
+      var userId = $sessionStorage.currentUser.id
+      var notebookLocation = BeakerUrlGeneratorService.fromParams({
+        uri: Restangular.one('notebooks', notebook.id).one('contents').getRestangularUrl(),
+        userId: userId,
+        projectId: notebook.projectId,
+        notebookId: notebook.id
+      });
 
-      $scope.notebookLocation = $sce.trustAsResourceUrl(notebookLocation(userId, prjId, notebook.id));
+      $scope.notebookLocation = $sce.trustAsResourceUrl(notebookLocation);
       $scope.notebook = notebook;
 
       Notebooks.update({id: notebook.id, open: true});
