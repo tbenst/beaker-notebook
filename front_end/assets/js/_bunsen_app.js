38f1fe1a0e60a1b93f1457acd9959bf3b1b6cc41
Carlos Gomes <carlos@mojotech.com>
2014-07-22 15:42:17 -0400

    breakup bunsen modules line

--- front_end/assets/js/_bunsen_app.js
+++ front_end/assets/js/_bunsen_app.js
@@ -1,5 +1,18 @@
 !(function(angular) {
-  window.bunsen = angular.module('bunsen', ['ui.router', 'ui.bootstrap', 'restangular', 'treeControl', 'marketPlaceFilters', 'truncate', 'ngStorage', 'beakerNotebook', 'angularFileUpload', 'stopEvent', 'filterMarketPlace', 'hiddenUpload'], ['RestangularProvider', function(RestangularProvider) {
-    RestangularProvider.setBaseUrl(window.BUNSEN_SERVICES.api);
-  }]);
+  window.bunsen = angular.module('bunsen',
+    ['ui.router',
+     'ui.bootstrap',
+     'restangular',
+     'treeControl',
+     'marketPlaceFilters',
+     'truncate',
+     'ngStorage',
+     'beakerNotebook',
+     'angularFileUpload',
+     'stopEvent',
+     'filterMarketPlace',
+     'hiddenUpload'
+    ], ['RestangularProvider', function(RestangularProvider) {
+      RestangularProvider.setBaseUrl(window.BUNSEN_SERVICES.api);
+    }]);
 })(angular);
