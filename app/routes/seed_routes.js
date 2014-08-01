6c98dcb1d0fbb3043d2e9a6f4a1e29205fef1056
Matthew Forsyth <matt@mojotech.com>
2014-07-31 21:59:24 -0400

    app: Change seed route to start with /api like other routes.
    This is necessary now that only /api is routed to the api
    by haproxy.

--- app/routes/seed_routes.js
+++ app/routes/seed_routes.js
@@ -5,14 +5,14 @@ var path        = require("path");
 var _           = require("lodash");
 
 module.exports = function(app) {
-  app.post("/seed/data", function(req, res, next) {
+  app.post("/api/seed/data", function(req, res, next) {
     Seed([req.body]).then(function(data) {
       res.json(data);
     })
     .catch(next);
   });
 
-  app.post("/seed/drop-repos", function(req, res, next) {
+  app.post("/api/seed/drop-repos", function(req, res, next) {
     exec("rm -rf " + path.resolve(__dirname, '../.repos'))
     .then(function() {
       res.send(200);
@@ -20,14 +20,14 @@ module.exports = function(app) {
     .catch(next);
   });
 
-  app.post("/seed/drop-all", function(req, res, next) {
+  app.post("/api/seed/drop-all", function(req, res, next) {
     Seed.dropAll().then(function() {
       res.send(200);
     })
     .catch(next);
   });
 
-  app.post("/seed/fetch", function(req, res, next) {
+  app.post("/api/seed/fetch", function(req, res, next) {
     app.Models[req.body.modelName].forge(req.body.data).fetch().then(function(d) {
       res.json(d);
     })
