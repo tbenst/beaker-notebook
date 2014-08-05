d5727afb3a710a8beaaa1cd2cc47f61ad462cc61
Jake Buob <jake@mojotech.com>
2014-08-05 16:06:45 -0400

    tests: add `var`

--- tests/widgets/show_dropdown.js
+++ tests/widgets/show_dropdown.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return this.Widgets.Dropdown = this.Widget.extend({
     show: function(el) {
-      dropdownSelectorShow = "arguments[0].classList.add('expanded')";
+      var dropdownSelectorShow = "arguments[0].classList.add('expanded')";
       return this.driver.executeScript(dropdownSelectorShow, el);
     }
   });
