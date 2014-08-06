6d48c117e308231c8d5b0559aabea69a58ac2595
Matthew Forsyth <matt@mojotech.com>
2014-08-06 14:12:15 -0400

    [tests] Do not provision new instances in test environment
    …but share the instance started by fig between all users.

--- provisioner/lib/docker_provisioner.js
+++ provisioner/lib/docker_provisioner.js
@@ -2,6 +2,9 @@ var _  = require('lodash');
 var Promise = require('bluebird');
 var Docker = require('dockerode');
 
+var BEAKER_PORT = '8801/tcp';
+var BEAKER_IMAGE = process.env.BEAKER_IMAGE;
+
 Promise.promisifyAll(require('dockerode/lib/docker').prototype);
 Promise.promisifyAll(require('dockerode/lib/container').prototype);
 
@@ -13,18 +16,35 @@ function DockerProvisioner(options) {
 
 _.extend(DockerProvisioner.prototype, {
   provision: function() {
-    return this.docker.createContainerAsync({
-      'Image': 'bunsen_beaker',
-      'ExposedPorts': {'8801/tcp': {}}
-    })
-    .then(function(container) {
-      // By not specyfying the host port when starting a container, we allow Docker to dynamically pick
-      // a free port available in the range 49153-65535
-      return container.startAsync({'PortBindings': {'8801/tcp': [{}]}})
-      .then(function() {
-        return inspectContainer(container);
+    var _this = this;
+
+    if (process.env['NODE_ENV'] != 'test') {
+      var exposedPorts = {};
+      exposedPorts[BEAKER_PORT] = {};
+      return this.docker.createContainerAsync({
+        'Image': BEAKER_IMAGE,
+        'ExposedPorts': exposedPorts
+      })
+      .then(function(container) {
+        // By not specyfying the host port when starting a container, we allow Docker to dynamically pick
+        // a free port available in the range 49153-65535
+        var portBindings = {};
+        portBindings[BEAKER_PORT] = [{}];
+        return container.startAsync({'PortBindings': portBindings})
+        .then(function() {
+          return inspectContainer(container);
+        });
       });
-    });
+    } else {
+      // We don't provision new instances in 'test' environment,
+      // but we share the instance started by fig between all users
+      return this.docker.listContainersAsync()
+      .then(function(list) {
+        var container = _.findWhere(list, {Image: BEAKER_IMAGE+':latest'})
+        return container.Id;
+      })
+      .then(_this.inspect.bind(_this));
+    }
   },
 
   inspect: function(id) {
@@ -38,7 +58,8 @@ function inspectContainer(container) {
   .then(function(data) {
     return {
       id: data.Id,
-      port: data.HostConfig.PortBindings['8801/tcp'][0]['HostPort']
+      port: data.HostConfig.PortBindings[BEAKER_PORT][0]['HostPort'],
+      running: data.State.Running
     }
   });
 }
