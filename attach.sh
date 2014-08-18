16818545566ce070170c99cbf0413c8028ee3353
Jake Buob <jake@mojotech.com>
2014-08-18 14:21:37 -0400

    Add shell script to attach to running docker containers

---
+++ attach.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -z $SSHED ]]; then
+  vagrant ssh -c "SSHED=1 /vagrant/attach.sh $1"
+else
+  PID="$(sudo docker inspect --format '{{.State.Pid}}' $1 2>/dev/null)"
+
+  if [[ -n $PID ]]; then
+    sudo nsenter --target "$PID" --mount --uts --ipc --net --pid
+  else
+    echo "$(tput bold)$(tput setaf 1)Couldn't find that container, are you sure it exists and is running?$(tput sgr0)"
+  fi
+fi
