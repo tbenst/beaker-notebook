9e827636d5b4eddf0f9f91e2c6272c75bd8752bc
Carlos Gomes <carlos@mojotech.com>
2014-08-21 14:30:30 -0400

    Update seed files

--- app/seed_files/data_tags.js
+++ app/seed_files/data_tags.js
@@ -1,164 +1,20 @@
 module.exports = [
   {
-    model: 'DataTag',
+    model: "DataTag",
     data: {
-      name: "Finance"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Energy"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "United States of America"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Canada"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Retail"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Crime"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Federal Government"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Global"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Credit Card"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "SEC"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "E-commerce"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "World Bank"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Rhode Island"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "OPM"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Population"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Gas"
-    }
-  },
-  {
-    model: 'DataTag',
-    data: {
-      name: "Electricity"
+      name: "Abstainers from Alcohol"
     }
   },
   {
     model: "DataTag",
     data: {
-      name: "Expenditure on Education"
+      name: "Age Limits"
     }
   },
   {
     model: "DataTag",
     data: {
-      name: "Education"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Mortality"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Main Health Indicators"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Health"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Life Expectancy"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Mortality Overview"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Prevalence of Disease"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Commodities"
-    }
-  },
-  {
-    model: "DataTag",
-    data: {
-      name: "Markets"
+      name: "Age Ranges for Educational Milestones"
     }
   },
   {
@@ -170,13 +26,787 @@ module.exports = [
   {
     model: "DataTag",
     data: {
+      name: "Alcohol Consumption"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Alcohol Costs"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Alcohol Finance"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Alcohol and Tobacco"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Alohol Related Crimes"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Aviation Gasoline"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Bagasse"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Balance of Payments"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Biodiesel"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Biogas"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Births and Deaths"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Bitumen Asphalt"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Blast Furnace Gas"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Bonds"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
       name: "By Category"
     }
   },
   {
     model: "DataTag",
     data: {
+      name: "By Exchange"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "CME"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Canada"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Capital Markets"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Charcoal"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Child Care"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Child Mortality Overview"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Coal"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Coke-oven Coke"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Commodities"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Consumption Per Capita by Beverage"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Consumption by Beverage"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Consumption by Use"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Credit Card"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Crime"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Crude Petroleum"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Culture"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Currencies"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Current Tobacco Smokers (crude)"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Daily Tobacco Smokers (age-standardized)"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Deaths Due to Disease"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Demographic Indicators"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Demography"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Density of Medical Personel"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Density of Medical Services"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Disease Prevention"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "E-commerce"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Economic Activity"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Economics"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Education"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Electricity"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Employment"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Energy"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Enrolment By Level of Education and Gender"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Enrolment Ratio by Gender"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Environment"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Equities"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Expenditure on Education"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Exports Breakdown"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Exposure"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Family"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Family Care"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Federal Government"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Finance"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Financial Derivatives"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Fuels/Energy Types"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Fuelwood"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
       name: "Futures"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Gas"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Gas-diesel Oils"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Gender"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Global"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Government Finances"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Grains"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Growth"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "HIV"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Health"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Health Effects of Alcohol"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Health Expenditure"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Health and Disease"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Housing"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Imports Breakdown"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Indicies"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Industry and Business"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Inflation"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Intake and Graduation Ratios"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "International Trade"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Life Expectancy"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Liquefied Petroleum Gas"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Main Health Indicators"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Markets"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Metals"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Military"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Money Supply"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Mortality"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Mortality Overview"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Natural Gas (including Lng)"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Natural Gas Liquids (ngl)"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Net Capacity by Plant Type"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "New York Taxi Cab Data, 2013"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Number of Health Workers"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Number of Teachers by Education Level"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "OPM"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Overview"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Percentage of Repeaters by Level of Education"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Politics"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Population"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Prevalence"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Prevalence of Disease"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Private Enrolment By Level Of Education"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Productive Sectors"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Quality of Life"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Rates"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Retail"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Rhode Island"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "SEC"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "School Age Population by Level of Education and Gender"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "School Life Expectancy by Level of Education"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Sexuality"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Society"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Softs"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Steam And Hot Water (gross"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Stocks"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Student Teacher Ratio by Education Level"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tax Structure"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Taxes on Most Sold Brand of Cigarettes as Percent of Price"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tertiary Education Enrolment by Subject"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tertiary Education Graduation by Subject"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tobacco"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tobacco Prices"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Tobacco Regulation"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Total Production by Energy Type"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "United States of America"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Uranium"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "World Bank"
+    }
+  },
+  {
+    model: "DataTag",
+    data: {
+      name: "Youth Rates"
     }
   }
 ]
