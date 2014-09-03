259aa96c6886253da69ce11c9b024b4f93df4fe1
Matthew Forsyth <matt@mojotech.com>
2014-09-03 11:43:45 -0400

    provisioner: gateway: Identify beakers by container name convention.

--- gateway/read_docker_beakers.sh
+++ gateway/read_docker_beakers.sh
@@ -6,10 +6,9 @@ function join { local IFS="$1"; shift; echo "$*"; }
 beakers=()
 
 while read -r cid; do
-  if [[ `docker inspect --format="{{.Config.Image}}" $cid` == $BEAKER_IMAGE ]]; then
+  if [[ `docker inspect --format="{{.Name}}" $cid` == "/${BEAKER_CONTAINER_PATTERN}"* ]]; then
     ip=${BEAKER_HOST-`docker inspect --format="{{.NetworkSettings.IPAddress}}" $cid`}
     port=${BEAKER_PORT-`docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}}{{end}}' $cid`}
-
     stanza=$(cat <<EOF
 {
   "name": "beaker_${cid}",
--- provisioner/main.sh
+++ provisioner/main.sh
@@ -2,7 +2,7 @@
 
 set -o nounset -o errexit -o pipefail
 
-export BEAKER_IMAGE=$1
+export BEAKER_CONTAINER_PATTERN=$1
 
 for i in "${@:2}"
 do
@@ -10,7 +10,12 @@ case $i in
   -h|--help)
     cat <<EOF
 
-  Usage: provisioner (beaker_image) [options]
+  Usage: provisioner (beaker_container_pattern) [options]
+
+  beaker_container_pattern tells the Provisioner how to name
+  and identify beaker containers.  Currently supported:  /beaker.
+  for development and /test.beaker for test environment.
+
   Options:
           -h  --help                  Display this message
 
