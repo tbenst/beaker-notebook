a2fe4139009dfcc16af82cb3a041fc680aca4741
Jake Buob <jake@mojotech.com>
2014-09-04 09:57:40 -0400

    frontend: Break controller dependencies to newlines

--- front_end/assets/js/controllers/_publications_list.js
+++ front_end/assets/js/controllers/_publications_list.js
@@ -1,9 +1,13 @@
 ;(function(app) {
-  app.controller('publicationsList', ['$scope', 'Factories', function($scope, Factories) {
-    var F = Factories;
+  app.controller('publicationsList', [
+    '$scope',
+    'Factories',
+    function($scope, Factories) {
+      var F = Factories;
 
-    F.Publications.getPublications().then(function(publications) {
-      $scope.publications.list = publications;
-    });
-  }]);
+      F.Publications.getPublications().then(function(publications) {
+        $scope.publications.list = publications;
+      });
+    }
+  ]);
 })(window.bunsen);
