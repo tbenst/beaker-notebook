a36f5b1a548b9ae522fc8394a08ea8d4cc6ead3e
Jake Buob <jake@mojotech.com>
2014-09-08 13:34:09 -0400

    db: Remove --psql option (and add readme)

---
+++ postgres/README.md
@@ -0,0 +1,10 @@
+Bunsen DB
+==========
+
+### (NOTICE: In order to run these commands in the docker containers, run `./attach.sh <containername>`)
+
+## Dev
+
+### Running `psql`
+  * `$ su postgres`
+  * `$ psql bunsenDevelopment`
--- postgres/main.sh
+++ postgres/main.sh
@@ -7,7 +7,6 @@ case $i in
   -c|--create) create=1 ;;
   -f|--force) force=1 ;;
   -r|--run) run=1 ;;
-  --psql) psql=1 ;;
   -h|--help)
     cat <<EOF
 
@@ -18,7 +17,6 @@ case $i in
           -f  --force     Before creating cluster, delete any existing old one
           --database=(db) Create a database with name db.
           -r  --run       Run the postgres server in the foreground.
-          --psql          Instead, open psql prompt
 EOF
     exit
     ;;
@@ -42,8 +40,4 @@ fi
 
 if [[ $run -eq 1 ]]; then
     exec su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /etc/postgresql/9.1/main"
-elif [[ $psql -eq 1 ]]; then
-    su postgres -c "/usr/lib/postgresql/9.1/bin/postgres -D /etc/postgresql/9.1/main &"
-    sleep 5
-    exec su postgres -c "psql bunsenDevelopment"
 fi
