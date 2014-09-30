4fa7b0c2709295b1ec9a381a3ab06b27f1572eca
Carlos Gomes <carlos@mojotech.com>
2014-09-30 14:37:55 -0400

    Use moment.js to convert date to UTC

--- front_end/assets/js/directives/_dataset_properties.js
+++ front_end/assets/js/directives/_dataset_properties.js
@@ -7,20 +7,12 @@
       },
       template: templates.dataset_properties,
       controller: ['$scope', function($scope) {
-        var toUTCDate = function(date){
-          var utc = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),  date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
-          return utc;
-        };
-
-        var millisToUTCDate = function(millis){
-          return toUTCDate(new Date(millis));
-        };
 
         var uiOmittedMetadata = ['csvPreview','description', 'numColumns', 'remoteFile', 'rows', 'startDate', 'tags','title'];
         $scope.item.filteredMeta = _.omit($scope.item.metadata, uiOmittedMetadata);
 
         if ($scope.item.metadata.startDate) {
-          $scope.item.metadata.startDate = millisToUTCDate($scope.item.metadata.startDate);
+          $scope.item.metadata.startDate = moment().utc($scope.item.metadata.startDate).format("M/DD/YY");
         }
 
         if ($scope.item.metadata.tags) {
