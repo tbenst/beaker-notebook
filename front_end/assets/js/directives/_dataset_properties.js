f94ee1dc37039775fd3462f44baefbb62e47be04
Carlos Gomes <carlos@mojotech.com>
2014-10-09 13:53:46 -0400

    Format date field to display properly

--- front_end/assets/js/directives/_dataset_properties.js
+++ front_end/assets/js/directives/_dataset_properties.js
@@ -8,14 +8,26 @@
       template: templates.dataset_properties,
       controller: ['$scope', function($scope) {
 
-        var specialUiAttrs = ['csvPreview', 'description', 'numColumns', 'remoteFile', 'rows', 'startDate', 'tags', 'title', 'dataPreviews', 'categories', 'vendor'];
-        var ignoreAttrs = ['subscriberIds', 'id', '_id', 'tabView', 'related']
+        var specialUiAttrs = ['csvPreview', 'description', 'numColumns', 'remoteFile', 'rows', 'tags', 'title', 'dataPreviews', 'categories', 'vendor'];
+        var ignoreAttrs = ['subscriberIds', 'id', '_id', 'tabView', 'related', 'catalog', 'index']
         var omitAttrs = specialUiAttrs.concat(ignoreAttrs);
 
-        $scope.item.filteredMeta = _.omit($scope.item, omitAttrs);
-        if ($scope.item.startDate) {
-          $scope.item.startDate = moment.utc($scope.item.startDate).local().format("M/D/YY");
+        function parseDate(dateField) {
+          $scope.item.filteredMeta[dateField] = moment.utc($scope.item[dateField]).local().format("M/D/YY");
         }
+
+        var dateFields = _.filter(_.keys($scope.item.catalog.metadata), function(key) {
+          return $scope.item.catalog.metadata[key].type == 'date'
+        })
+
+        $scope.item.filteredMeta = _.omit($scope.item, omitAttrs);
+
+        _.each(dateFields, function (dateField) {
+          if ($scope.item[dateField]) {
+            parseDate(dateField);
+          }
+        })
+
       }]
     }
   });
