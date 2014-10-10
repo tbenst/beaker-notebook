tbenst/beaker-notebook

commit d7f21138cb6e8ee2511b60b3299ef474b06def89

front_end: Show current catalog's filters in the UI

front_end/assets/js/controllers/_market_nav.js
@@ -12,19 +12,19 @@
     }
 
     $scope.searchByTag = function(tag) {
-      $scope.newSearch({tagScope: [tag.id.toString()]});
+      $scope.newSearch({dataTagsScope: [tag]});
     };
 
     $scope.isTagSelected = function(tag) {
-      return _.contains($scope.marketPlace.tagScope, tag.id.toString());
+      return _.contains($scope.marketPlace.tagsScope, tag);
     };
 
     $scope.checkRelatedSetHeight = function() {
-      var relatedTags = $scope.marketPlace.relatedTags;
-      var relatedTagsToDisplay = 6;
+      var dataTags = $scope.marketPlace.tags;
+      var dataTagsToDisplay = 6;
 
-      if(relatedTags != undefined) {
-        return relatedTags.length >= relatedTagsToDisplay;
+      if(dataTags != undefined) {
+        return dataTags.length >= dataTagsToDisplay;
       }
     }
 

front_end/assets/js/controllers/_market_place.js
@@ -3,37 +3,6 @@
   app.controller('marketPlace', ['$q', '$scope', 'Factories', 'TagNormalizeService', function($q, $scope, Factories, TagNormalizeService) {
     var F = Factories;
 
-    $scope.isItemSelected = function(item, modelScope) {
-      item = item.id || item;
-
-      if(!_.isArray(modelScope)) {
-        modelScope = $scope.$eval(modelScope);
-      }
-
-      if(modelScope) {
-        return modelScope.indexOf(item) != -1;
-      } else {
-        return false;
-      }
-    }
-
-    $scope.selectItem = function(item, itemStore) {
-      var itemStoreName  = itemStore;
-
-      itemStore   = $scope.$eval(itemStore) || [];
-      var itemKey = item.id ? item.id : item;
-
-      if(!$scope.isItemSelected(itemKey, itemStore)) {
-        itemStore.push(itemKey);
-      } else {
-        var removeIndex = itemStore.indexOf(itemKey);
-        if (removeIndex != -1) {
-          itemStore.splice(removeIndex, 1);
-        }
-      }
-      $scope.$eval(itemStoreName+"="+JSON.stringify(itemStore));
-    }
-
     $scope.removeFilter = function(value, model) {
       _.remove($scope.$eval(model), function(v) {
         return v === value;
@@ -46,11 +15,13 @@
       var TS = TagNormalizeService;
 
       // condense all search filters into a flat list
-      return Array.prototype.concat(
-        TS.normalizeTypes($scope.marketPlace.typeScope, 'marketPlace.typeScope', $scope),
-        TS.normalizeVendors($scope.marketPlace.vendorScope, 'marketPlace.vendorScope', $scope),
-        TS.normalizeTags($scope.marketPlace.tagScope, 'marketPlace.tagScope', $scope)
-      );
+      var scopes = _($scope.marketPlace.filters).keys().map(function(f) {
+        var filterScope = f + 'Scope';
+        return TS.normalizeFilter($scope.marketPlace[filterScope], 'marketPlace.' + filterScope, $scope)
+      })
+      .value();
+
+      return Array.prototype.concat.apply([], scopes);
     }
 
     function checkDataSets(newValue, oldValue) { if (newValue !== oldValue) getDataSets(); }
@@ -93,18 +64,32 @@
       $scope.marketPlace.currentPage = 1;
     }
 
-
     if (!$scope.marketPlace.data) {
       getDataSets();
     }
 
+    function filters() {
+      return _.keys($scope.marketPlace.filters);
+    }
+
     $scope.$watch('marketPlace.currentPage', changePage);
-    $scope.$watchCollection('marketPlace.typeScope', resetDataSets);
-    $scope.$watchCollection('marketPlace.vendorScope', resetDataSets);
-    $scope.$watchCollection('marketPlace.tagScope', resetDataSets);
     $scope.$watch('marketPlace.searchTerm', resetDataSets);
     $scope.$watch('marketPlace.searchScope', resetDataSets);
     $scope.$watch('marketPlace.currentCategory', checkDataSets);
+    $scope.$watch(filters, watchFilterScopes, true);
+
+    var watchers = [];
+    function unbindWatchers() {
+      _.invoke(watchers, 'call');
+      watchers = [];
+    }
+
+    function watchFilterScopes() {
+      unbindWatchers();
+      _($scope.marketPlace.filters).keys().each(function(f) {
+        watchers.push($scope.$watchCollection('marketPlace.' + f + 'Scope', resetDataSets));
+      })
+    }
   }]);
 
 })(angular, window.bunsen);

front_end/assets/js/controllers/_market_place_root.js
@@ -9,7 +9,11 @@
     };
 
     function clearSearch() {
-      var deleteList = ['categoryPath', 'vendorScope', 'typeScope', 'tagScope', 'searchTerm', 'searchScope'];
+      var filterScopes = _($scope.marketPlace.filters).keys().map(function(f) {
+        return f + 'Scope';
+      }).value()
+
+      var deleteList = _.union(['categoryPath', 'searchTerm', 'searchScope'], filterScopes);
       _.each(deleteList, function(i) {
         delete $scope.marketPlace[i];
       });

front_end/assets/js/directives/_filter_controls.js
@@ -4,12 +4,31 @@
     return {
       restrict: 'E',
       template: templates.multi_select,
-      compile: function(element, attrs)
-      {
-        var type = attrs.type || 'text';
-        attrs.iteratorDisplayName = attrs.iteratorDisplayName || attrs.iteratorName;
-        element.replaceWith(this.template(attrs));
+      scope: {
+        filterScope: '=',
+        collection: '='
+      },
+      link: function(scope, element, attrs) {
+
+        scope.isItemSelected = function(item) {
+          if (scope.filterScope) {
+            return scope.filterScope.indexOf(item) != -1;
+          }
+        }
+
+        scope.selectItem = function(item) {
+          if(!scope.isItemSelected(item)) {
+            scope.filterScope = scope.filterScope || [];
+            scope.filterScope.push(item);
+          } else {
+            var removeIndex = scope.filterScope.indexOf(item);
+            if (removeIndex != -1) {
+              scope.filterScope.splice(removeIndex, 1);
+            }
+          }
+        }
       }
+
     }
   });
 })(angular);

front_end/assets/js/factories/_data_sets.js
@@ -6,29 +6,24 @@
       limit: scope.itemsPerPage
     };
 
-    if (scope.vendorScope !== void(0) && scope.vendorScope.length > 0) {
-      query.vendorIDs = encodeURIComponent(scope.vendorScope.join(","));
-    }
-
-    if (scope.categoryPath !== void(0)) {
-      query.categoryPath = encodeURIComponent(scope.categoryPath);
-    }
-
-    if (scope.tagScope !== void(0) && scope.tagScope.length > 0) {
-      query.tagIDs = encodeURIComponent(scope.tagScope.join(','));
-    }
-
-    if (scope.typeScope !== void(0) && scope.typeScope.length > 0) {
-      query.formats = encodeURIComponent(scope.typeScope.join(','));
-    }
-
     if (scope.searchTerm !== void(0) && scope.searchTerm.length > 0) {
-      query.searchTerm = encodeURIComponent(scope.searchTerm);
+      query.searchTerm = scope.searchTerm;
     }
 
     if (scope.searchScope !== void(0) && scope.searchScope.length > 0) {
-      query.searchScope = encodeURIComponent(scope.searchScope);
+      query.searchScope = scope.searchScope;
     }
+
+    if (scope.categoryPath !== void(0)) {
+      query.categoryPath = scope.categoryPath;
+    }
+
+    _(scope.filters).keys().each(function(f) {
+      var s = scope[f + 'Scope'];
+      if (s !== void(0) && s.length > 0) {
+        query[f] = s;
+      }
+    })
 
     return query;
   }

front_end/assets/js/services/_tag_normalize_service.js
@@ -1,29 +1,10 @@
 ;(function(app) {
   app.service('TagNormalizeService', function() {
     return {
-      normalizeTypes: function(types, modelName, $scope) {
-        return _.map(types, function(v) {
+      normalizeFilter: function(filterValues, modelName, $scope) {
+        return _.map(filterValues, function(v) {
           return {
             "name": v,
-            "value": v,
-            "model": modelName
-          }
-        });
-      },
-      normalizeVendors: function(vendors, modelName, $scope) {
-        return _.map(vendors, function(vendorID) {
-          return {
-            "name": _.findWhere($scope.marketPlace.vendors, {id: +vendorID}).name,
-            "value": vendorID,
-            "model": modelName
-          }
-        });
-      },
-      normalizeTags: function(tags, modelName, $scope) {
-        return _.map(tags, function(tagID) {
-          return {
-            "name": _.findWhere($scope.marketPlace.relatedTags, {id: +tagID}).name,
-            "value": tagID,
             "model": modelName
           }
         });

front_end/assets/templates/market_nav.jade
@@ -11,12 +11,12 @@ input.search.full(ng-model="marketPlace.searchTerm", ng-model-options="{ debounc
     item: {{node.category}}
     span.count {{node.count}}
 
-.sidebar-box.sidebar-related-tags(ng-if="marketPlace.relatedTags")
+.sidebar-box.sidebar-related-tags(ng-if="marketPlace.filters.tags")
   h3 related sets
   .content
     dl.related-tags(ng-class="expanded ? '' : 'related-tags-hidden'")
-      dt.related-tag(ng-repeat="tag in marketPlace.relatedTags")
-        a.tag-name(ng-click="searchByTag(tag)") {{tag.name}}
+      dt.related-tag(ng-repeat="tag in marketPlace.filters.tags")
+        a.tag-name(ng-click="searchByTag(tag)") {{tag}}
         span(ng-show='isTagSelected(tag)') *
     .related-tags-visibility
       a.related-tags-show-all.right(ng-click="$parent.expanded = !$parent.expanded", ng-if="checkRelatedSetHeight()")

front_end/assets/templates/market_place.jade
@@ -25,32 +25,21 @@
     .filter-header.filter-col
       .filter-segment-label filters
 
-    .filter-segment.filter-col.filter-formats
+
+
+    .filter-segment.filter-col(ng-repeat="(filter, values) in marketPlace.filters" ng-class="'filter-' + filter")
       .filter-segment-label
-        | format
+        | {{filter}}
         img(src="images/down-arrow-filter.png", width="6", height="4")
+      multi-filter(filter-scope="marketPlace[filter + 'Scope']", collection="values")
 
-      multi-filter(ng-model="marketPlace.typeScope", iterator="format in marketPlace.formats", iterator-name="format")
 
-    .filter-segment.filter-col.filter-vendors
-      .filter-segment-label
-        | vendor
-        img(src="images/down-arrow-filter.png", width="6", height="4")
-
-      multi-filter(ng-model="marketPlace.vendorScope", iterator="vendor in marketPlace.vendors", iterator-name="vendor", iterator-display-name="vendor.name")
-
-    .filter-segment.filter-col.filter-tags
-      .filter-segment-label
-        | tags
-        img(src="images/down-arrow-filter.png", width="6", height="4")
-
-      multi-filter(ng-model="marketPlace.tagScope", iterator="tag in marketPlace.relatedTags", iterator-name="tag", iterator-display-name="tag.name")
 
     .filter-segment.filter-search.filter-col
       input.search.full.market-text-search(type="text", ng-model="marketPlace.searchScope", placeholder="Search within results", ng-model-options="{ debounce: {'default': 500, 'blur': 0} }")
 
   ul.current-filters
-    li.tag.filter(ng-repeat="filter in currentFilters", ng-click="removeFilter(filter.value, filter.model)") {{filter.name}}
+    li.tag.filter(ng-repeat="filter in currentFilters", ng-click="removeFilter(filter.name, filter.model)") {{filter.name}}
 
   ul.market-list.bunsen-list
     li.bunsen-list-item(ng-repeat="item in marketPlace.data")
@@ -60,7 +49,7 @@
         p.description {{item.description | limitTo:100}} {{item.description.length > 100 ? "..." : ""}}
         .vendor
           | Vendor:&nbsp;
-          span.vendors {{item.vendor.name}}
+          span.vendors {{item.vendor}}
       .col-quarter
         ul.attributes
           li

commit cbc218e1907572984912883ca25d67c9c3cd4521

Fix incorrect dataTagName => tagName rename.
Looks like this was not changed in
d7f21138cb6e8ee2511b60b3299ef474b06def89

which introduced this fun bug.

front_end/assets/js/controllers/_market_nav.js
@@ -34,7 +34,7 @@
     }
 
     $scope.searchByTag = function(tag) {
-      $scope.newSearch({dataTagsScope: [tag]});
+      $scope.newSearch({tagsScope: [tag]});
     };
 
     $scope.isTagSelected = function(tag) {