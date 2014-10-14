be98bd810bcab7b35b6383e5402d8d0462f224db
grin <bartek@mojotech.com>
2014-10-14 21:45:36 +0200

    tests: Add a delay to the failing catalog step

--- tests/step_definitions/market_steps.js
+++ tests/step_definitions/market_steps.js
@@ -440,7 +440,10 @@ module.exports = function() {
 
   this.When(/^I browse "([^"]*)" catalog$/, function(catalog) {
     var marketCategory = new this.Widgets.MarketCategory();
-    return marketCategory.clickCategory(catalog);
+    return bluebird.delay(1500)
+    .then(function() {
+      return marketCategory.clickCategory(catalog);
+    })
   });
 
   this.Then(/^I should be in "([^"]*)" catalog$/, function(catalog) {
