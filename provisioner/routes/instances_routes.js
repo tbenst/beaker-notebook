1870cea6eeaf802e278461305d5b4cba4a873a50
Will Farrell <will@mojotech.com>
2014-10-24 14:37:27 -0400

    [provisioner] Remove node.js provisioner implementation.

--- provisioner/routes/instances_routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-
-  var InstancesController = app.Controllers.InstancesController;
-
-  app.post('/beaker/instances', InstancesController.create);
-  app.get('/beaker/instances/:id', InstancesController.get);
-  app.put('/beaker/instances/:id', InstancesController.update);
-};
