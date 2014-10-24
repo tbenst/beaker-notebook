07d58c46a8229145899ba7f1e48c7b6108225c63
Chris Shoemaker <chris@mojotech.com>
2014-10-23 20:27:28 -0400

    [tests] Expose the display via vnc.

--- tests/main.sh
+++ tests/main.sh
@@ -3,7 +3,9 @@
 export DISPLAY=:99.0
 
 # start xdisplay
-Xvfb :99 -shmem -screen 0 1366x768x16 &
+Xvfb :99 -shmem -screen 0 1368x768x16 &
+fluxbox &
+x11vnc -shared -display :99 -bg -nopw -listen 0.0.0.0 -forever -logappend /var/log/x11vnc.log -xkb
 
 # run the test
 java -jar selenium.jar &
