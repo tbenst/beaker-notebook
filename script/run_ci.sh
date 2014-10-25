a70876a69ecf419fae1a1609643d9c3b0379f51d
Chris Shoemaker <chris@mojotech.com>
2014-10-25 12:53:18 -0400

    [test] Teach the test container how to wait for bunsen to be up before testing.

--- script/run_ci.sh
+++ script/run_ci.sh
@@ -13,9 +13,4 @@ docker ps -a -q | xargs --no-run-if-empty docker rm
 
 ./script/run.sh --env=ci
 
-until $(curl -o /dev/null -s --head --fail -H "Host:gateway" http://u:p@localhost:7778); do
-  echo 'waiting for gateway server...'
-  sleep 5
-done
-
 docker run --link=ci.gateway:gateway --name ci.test -p 5900:5900 -e BUNSEN_HOSTNAME=gateway test
--- tests/main.sh
+++ tests/main.sh
@@ -7,6 +7,15 @@ Xvfb :99 -shmem -screen 0 1368x768x16 &
 fluxbox &
 x11vnc -shared -display :99 -bg -nopw -listen 0.0.0.0 -forever -logappend /var/log/x11vnc.log -xkb
 
+BUNSEN_HOSTNAME=${BUNSEN_HOSTNAME-localhost}
+
+# wait for app to be ready
+bunsen_url=http://u:p@${GATEWAY_PORT_80_TCP_ADDR}:${GATEWAY_PORT_80_TCP_PORT}
+until $(curl -o /dev/null -s --head --fail -H "Host:${BUNSEN_HOSTNAME}" $bunsen_url); do
+  echo "waiting for bunsen at ${bunsen_url} ..."
+  sleep 1
+done
+
 # run the test
 java -jar selenium.jar &
 npm start
