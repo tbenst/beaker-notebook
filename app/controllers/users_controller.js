cd913ac45a0d8c5ef578df47a5ec294baeb4bf18
Carlos Gomes <carlos@mojotech.com>
2014-10-28 17:30:46 -0400

    Add get and update functions to express users controller

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -14,6 +14,28 @@ module.exports = function(app) {
         .then(function() {
           res.json(req.user);
         }).catch(next);
+    },
+
+    get: function (req, res, next) {
+      res.json(app.Models.User.sanitize(req.user))
+    },
+
+    update: function (req, res, next) {
+      var attrs = _.pick(req.body, 'name', 'email', 'currentPassword', 'newPassword');
+      req.user.update(attrs)
+        .then(function (user) {
+          res.json(user);
+        })
+        .catch(function (err) {
+          var statusCode = 500;
+          if (err.errors && err.errors.email) {
+            err.message = err.errors.email.message;
+            statusCode = 422;
+          } else if (err.message == 'Wrong Password') {
+            statusCode = 422;
+          }
+          res.status(statusCode).send(err.message);
+        });
     }
   }
 }
