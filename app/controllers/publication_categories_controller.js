50bf47f39035d0785d2fcd920b286e47cc71948d
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-11-06 13:14:10 -0500

    Add endpoint for getting cat by ID

--- app/controllers/publication_categories_controller.js
+++ app/controllers/publication_categories_controller.js
@@ -2,10 +2,22 @@ module.exports = function(app) {
   var PublicationCategory = app.Models.PublicationCategory;
 
   return {
+    categoryIdParam: function(req, res, next, id) {
+      PublicationCategory.forge({id: id})
+      .fetch()
+      .then(function(record) {
+        if (!record) throw new Error('Publication Category not found ');
+        req.category = record
+      })
+      .done(next, next);
+    },
     index: function(req, res, next) {
       PublicationCategory.fetchAll()
       .then(res.json.bind(res))
       .catch(next);
+    },
+    get: function(req, res, next) {
+      res.json(req.category);
     }
   }
 };
