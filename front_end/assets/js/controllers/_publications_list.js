e7089d172fbbedd7d3f503356b0c5b8ab4995ea7
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-11-06 13:15:34 -0500

    Expose publication category and quantity to scope

--- front_end/assets/js/controllers/_publications_list.js
+++ front_end/assets/js/controllers/_publications_list.js
@@ -5,10 +5,16 @@
     'Factories',
     function($scope, $stateParams, Factories) {
       var F = Factories;
+      var categoryID = $stateParams.category_id;
 
       F.Publications.getPublications($stateParams).then(function(publications) {
-        $scope.publications.currentCategory = $stateParams.category_id;
+        $scope.publications.currentCategory = categoryID;
         $scope.publications.list = publications;
+        $scope.publications.quantity = publications.length;
+      });
+
+      F.PublicationCategories.getCategory(categoryID).then(function(category) {
+        $scope.publications.category = (categoryID !== null) ? category : null;
       });
     }
   ]);
