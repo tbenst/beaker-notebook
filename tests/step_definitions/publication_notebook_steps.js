259a1aca206c9b63c6b6115dcce35de03aee3e78
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-11-17 10:14:33 -0500

    Create widget for publication notebook hero and update/define steps

--- tests/step_definitions/publication_notebook_steps.js
+++ tests/step_definitions/publication_notebook_steps.js
@@ -8,8 +8,16 @@ module.exports = function() {
         description: "",
         contents: "{\"beaker\":\"2\",\"evaluators\":[{\"name\":\"Html\",\"plugin\":\"Html\",\"view\":{\"cm\":{\"mode\":\"htmlmixed\"}}},{\"name\":\"Latex\",\"plugin\":\"Latex\",\"view\":{\"cm\":{\"mode\":\"stex\"}}},{\"name\":\"IPython\",\"plugin\":\"IPython\",\"imports\":\"\",\"supplementalClassPath\":\"\"},{\"name\":\"JavaScript\",\"plugin\":\"JavaScript\",\"jsSetting2\":\"\",\"jsSetting1\":\"\",\"view\":{\"cm\":{\"mode\":\"javascript\",\"background\":\"#FFE0F0\"}}},{\"name\":\"Node\",\"plugin\":\"Node\",\"view\":{\"cm\":{\"mode\":\"javascript\"}}}],\"cells\":[{\"id\":\"section001\",\"type\":\"section\",\"level\":1,\"title\":\"Hello Beaker\",\"collapsed\":false,\"evaluatorReader\":false},{\"id\":\"section7np9PW\",\"type\":\"section\",\"title\":\"New Section H2\",\"level\":2,\"evaluatorReader\":false,\"collapsed\":false},{\"id\":\"text6v9PwO\",\"type\":\"text\",\"body\":\"this is a text cell\",\"evaluatorReader\":false},{\"id\":\"sectionCY4fCm\",\"type\":\"section\",\"title\":\"New Section H3\",\"level\":3,\"evaluatorReader\":false,\"collapsed\":false},{\"id\":\"markdown3WdhVr\",\"type\":\"markdown\",\"body\":\"### ok\\n## this\\n# is\\n__markdown__\",\"evaluatorReader\":false,\"mode\":\"preview\"},{\"id\":\"codeZhXco4\",\"type\":\"code\",\"evaluator\":\"Node\",\"input\":{\"body\":\"1+1 //node\"},\"output\":{\"selectedType\":\"Text\",\"state\":{},\"result\":\"2\"},\"evaluatorReader\":true,\"lineCount\":1},{\"id\":\"sectionT9MNJ0\",\"type\":\"section\",\"title\":\"New Section H4\",\"level\":4,\"evaluatorReader\":false,\"collapsed\":false},{\"id\":\"code301W1U\",\"type\":\"code\",\"evaluator\":\"JavaScript\",\"input\":{\"body\":\"25*2\"},\"output\":{\"selectedType\":\"Text\",\"state\":{},\"result\":\"50\"},\"evaluatorReader\":true,\"lineCount\":1},{\"id\":\"text97Oipt\",\"type\":\"text\",\"body\":\"<p style=\\\"margin-bottom: 15px; color: rgb(0, 0, 0); font-family: Helvetica, arial, freesans, clean, sans-serif; font-size: 15px; line-height: 20px;\\\">Beaker also supports <b>Julia</b>, <b>Groovy</b>, <b>Ruby</b>, and <b>Node</b>. To add another language to this notebook, select <b>Notebook</b> ▶ <b>Plugin manager</b> from the menu on top of the screen.</p><p style=\\\"margin-top: 15px; color: rgb(0, 0, 0); font-family: Helvetica, arial, freesans, clean, sans-serif; font-size: 15px; line-height: 20px; margin-bottom: 0px !important;\\\">For general help, see the tutorial notebook under <b>Help</b> ▶ <b>Tutorial notebook</b></p>\",\"evaluatorReader\":false}],\"namespace\":{}}",
         categoryId: 0
-      }
-    })
+      },
+      associations: [
+        {
+          foreignKey: "userId",
+          lookup: {
+            User: {email: "u@r.edu"}
+          }
+        }
+      ]
+    });
   });
 
   this.When(/^I view the publication$/, function() {
@@ -28,6 +36,10 @@ module.exports = function() {
       })
     }.bind(this), global.timeout)
     .should.eventually.eql(true);
+  });
+
+  this.Then(/^I should see the author "([^"]*)"$/, function(author) {
+    return new this.Widgets.PublicationNotebookHero().author().should.eventually.eql(author);
   });
 
   this.When(/^I should be able to collapse and expand inputs$/, function() {
