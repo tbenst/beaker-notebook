ca182e94c4e141791d96a2a99efd6a488821b6b4
Sam Saccone <samccone@gmail.com>
2014-11-17 15:27:47 -0500

    [Style] Split authentication DI to multiline.

--- front_end/assets/js/routes/_authentication.js
+++ front_end/assets/js/routes/_authentication.js
@@ -1,40 +1,45 @@
 !(function(angular, app, templates) {
-  app.config(['$stateProvider', '$urlRouterProvider', function setRoutes($stateProvider, $urlRouterProvider) {
+  app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function setRoutes(
+      $stateProvider,
+      $urlRouterProvider) {
 
-    $stateProvider
-      .state('signIn', {
-        url: '/sign_in',
-        skipAuth: true,
-        views: {
-          root: {
-            controller: 'authentication',
-            template: templates.sign_in
-          }
-        }
-      })
-      .state('signOut', {
-        url: '/sign_out',
-        skipAuth: true,
-        onEnter: ['$rootScope', function($rootScope) {
-          $rootScope.signOut();
-        }],
-        views: {
-          root: {
-            controller: 'authentication',
-            template: templates.sign_out
-          }
-        }
-      })
-      .state('signUp', {
-        url: '/sign_up',
-        skipAuth: true,
-        views: {
-          root: {
-            controller: 'authentication',
-            template: templates.sign_up
-          }
-        }
-      });
-
-  }]);
+        $stateProvider
+          .state('signIn', {
+            url: '/sign_in',
+            skipAuth: true,
+            views: {
+              root: {
+                controller: 'authentication',
+                template: templates.sign_in
+              }
+            }
+          })
+          .state('signOut', {
+            url: '/sign_out',
+            skipAuth: true,
+            onEnter: ['$rootScope', function($rootScope) {
+              $rootScope.signOut();
+            }],
+            views: {
+              root: {
+                controller: 'authentication',
+                template: templates.sign_out
+              }
+            }
+          })
+          .state('signUp', {
+            url: '/sign_up',
+            skipAuth: true,
+            views: {
+              root: {
+                controller: 'authentication',
+                template: templates.sign_up
+              }
+            }
+          });
+    }]
+  );
 })(angular, window.bunsen, templates);
