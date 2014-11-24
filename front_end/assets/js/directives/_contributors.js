ffacab86ae70b7415e018df2d026c183304b2e0d
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-11-23 21:44:36 -0500

    Scope category ID & serve contributors off ID value

--- front_end/assets/js/directives/_contributors.js
+++ front_end/assets/js/directives/_contributors.js
@@ -2,10 +2,21 @@
   app.directive('contributors', ['Factories', function(F) {
     return {
       restrict: 'E',
+      scope: {
+        categoryId: '='
+      },
       template: templates.contributors,
       link: function(scope, element) {
-        F.Users.getContributors().then(function(contributors) {
-          scope.contributors.list = contributors;
+        scope.$watch('categoryId', function(categoryID) {
+          if(categoryID !== null) {
+            F.Users.getContributorsByCat(categoryID).then(function(contributors) {
+              scope.contributors = contributors;
+            });
+          } else {
+            F.Users.getContributors().then(function(contributors) {
+              scope.contributors = contributors;
+            });
+          }
         });
       }
     }
