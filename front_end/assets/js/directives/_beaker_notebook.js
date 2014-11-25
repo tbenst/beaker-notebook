22f93ce4aace37005f723729f69b8fe833b32009
Seth Krasnianski <sethkrasnianski@gmail.com>
2014-11-25 10:39:27 -0500

    Refactor directive to be more explicit by using scope

--- front_end/assets/js/directives/_beaker_notebook.js
+++ front_end/assets/js/directives/_beaker_notebook.js
@@ -3,35 +3,50 @@
   .directive('beakernotebook', function() {
     return {
       restrict: 'E',
-      link: function(scope, element, attrs) {
-        attrs.$observe("src", function(v) {
-
-          var frameId = 'beaker-frame-'+attrs.notebookId;
-          var frameSelector = '#'+frameId;
-
-          if (v && v.length) {
-            if (cached = document.querySelector(frameSelector)) {
-              cached.style.display="block";
-            } else {
-              var frame = document.createElement("iframe")
-              frame.setAttribute('id', frameId);
-              frame.src = v;
-              frame.setAttribute('width', attrs.width);
-              frame.setAttribute('height', attrs.height);
-              frame.setAttribute('scrolling', 'no');
-              frame.setAttribute('class', 'beaker');
-              document.getElementById('beaker-container').appendChild(frame);
-            }
-
-            element.on('$destroy', function() {
-              var frame = document.querySelector(frameSelector);
-
-              if (frame) {
-                frame.style.display="none";
-              }
-            });
+      scope: {
+        notebook: "=",
+        width: "@",
+        height: "@"
+      },
+      link: function(scope, element) {
+        scope.$watch("notebook", function(nu, old) {
+          if (nu == void 0) {
+            return ;
           }
-        })
+
+          var notebook      = scope.notebook.current;
+          var id            = notebook.id;
+          var location      = notebook.location;
+          var frameId       = 'beaker-frame-'+id;
+          var frameSelector = '#'+frameId;
+          var cached        = document.querySelector(frameSelector);
+
+          if (location == void 0 && cached == void 0) {
+            return ;
+          }
+
+          if (cached) {
+            cached.style.display="block";
+          } else {
+            var frame = document.createElement("iframe")
+            frame.setAttribute('id', frameId);
+            frame.src = location.toString();
+            frame.setAttribute('width', scope.width);
+            frame.setAttribute('height', scope.height);
+            frame.setAttribute('scrolling', 'no');
+            frame.setAttribute('class', 'beaker');
+            document.getElementById('beaker-container').appendChild(frame);
+          }
+
+          element.on('$destroy', function() {
+            var frame = document.querySelector(frameSelector);
+
+            if (frame) {
+              frame.style.display="none";
+            }
+          });
+
+        }, true)
       }
     }
   });
