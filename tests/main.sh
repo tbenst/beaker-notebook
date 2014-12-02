706ef89a357247b195cd4f6dcd488703db8e23fb
Will Farrell <will@mojotech.com>
2014-12-02 17:39:57 -0500

    [tests] Cleanup and modify how we get coverage stats.

--- tests/main.sh
+++ tests/main.sh
@@ -1,30 +1,48 @@
+#!/usr/bin/env bash
+
 # export display so firefox knows where to show
 # since we are in headless mode
 export DISPLAY=:99.0
+export BUNSEN_HOSTNAME=${BUNSEN_HOSTNAME-localhost}
 
-# start xdisplay
-Xvfb :99 -shmem -screen 0 1368x768x16 &
-fluxbox &
-x11vnc -shared -display :99 -bg -nopw -listen 0.0.0.0 -forever -logappend /var/log/x11vnc.log -xkb
+function main {
+  local code=0
+  local help=\
+"Usage: web [options]
 
-BUNSEN_HOSTNAME=${BUNSEN_HOSTNAME-localhost}
+Options:
+  -h,  --help          Display this message
+  -c,  --coverage      Collect coverage report"
 
-# wait for app to be ready
-bunsen_url=http://u:p@${GATEWAY_PORT_80_TCP_ADDR}:${GATEWAY_PORT_80_TCP_PORT}
-until $(curl -o /dev/null -s --head --fail -H "Host:${BUNSEN_HOSTNAME}" $bunsen_url); do
-  echo "waiting for bunsen at ${bunsen_url} ..."
-  sleep 1
-done
+  for i in "$@"; do
+    case $i in
+      -h|--help) echo "$help"; exit 1 ;;
+      -c|--coverage) ENABLE_COVERAGE=y ;;
+    esac
+    shift
+  done
 
-# run the test
-java -jar selenium.jar &
-npm start
-features_return_code=$?
+  # start xdisplay
+  Xvfb :99 -shmem -screen 0 1368x768x16 &
 
-# download test coverage
-coverage_dir=/var/app/test/coverage
-mkdir -p $coverage_dir
-curl -o "${coverage_dir}/client_coverage.zip" -H "Host:${BUNSEN_HOSTNAME}" $bunsen_url/coverage/download
-curl -o "${coverage_dir}/api_coverage.zip" -H "Host:${BUNSEN_HOSTNAME}" $bunsen_url/api/coverage/download
+  # start window manager
+  fluxbox &
 
-exit $features_return_code
+  # start vnc server
+  x11vnc -shared -display :99 -bg -nopw -listen 0.0.0.0 -forever -logappend /var/log/x11vnc.log -xkb
+
+  # start selenium
+  java -jar selenium.jar &
+
+  # run tests
+  npm start; code=$?
+
+  if [[ -n $ENABLE_COVERAGE ]]; then
+    curl -o "coverage.json" $BUNSEN_HOSTNAME/api/coverage/object
+    npm run coverage
+  fi
+
+  exit $code
+}
+
+main "$@"
--- tests/util/coverage.js
+++ tests/util/coverage.js
@@ -4,7 +4,7 @@ module.exports = function() {
   this.AfterAll(function() {
     // post back to the front_end server to report coverage
     var _this = this;
-    return this.driver.executeScript('angular.element(document).injector().get("$http").post("/coverage/client", JSON.stringify(window.__coverage__));')
+    return this.driver.executeScript('angular.element(document).injector().get("$http").post("/api/coverage/client", JSON.stringify(window.__coverage__));')
       .then(function() {
 	return _this.driver.sleep(100);
       });
