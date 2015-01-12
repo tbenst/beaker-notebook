9702c6ce62da825082744922036591bbd696e09f
Seth Krasnianski <seth@mojotech.com>
2015-01-12 15:21:06 -0500

    front_end: Update tracking service to post measurements to events endpoint

--- front_end/assets/js/services/_tracking_service.js
+++ front_end/assets/js/services/_tracking_service.js
@@ -1,4 +1,24 @@
 ;(function(app) {
+
+  function beautifyName(name) {
+    return name.match(/[A-Z][a-z]+/g).join(" ");
+  }
+
+  function msToSeconds(time) {
+    return time / 1000;
+  }
+
+  function buildPostData(measurement) {
+    return {
+      host: "bunsen",
+      service: "http req",
+      metric: msToSeconds(measurement.duration),
+      state: "ok",
+      description: beautifyName(measurement.name) + " took " + msToSeconds(measurement.duration) + ".",
+      tags: ['http', 'librato']
+    };
+  }
+
   app.service('TrackingService', [
     'Factories',
     function(F) {
@@ -7,13 +27,14 @@
         performance.clearMarks(name);
         performance.mark(name);
         var mark = performance.getEntriesByName(name)[0];
-        console.log(mark);
       },
+
       measure: function(name, from, to) {
         if(performance.getEntriesByName(from).length && performance.getEntriesByName(to).length) {
           performance.measure(name, from, to);
           var measurement = performance.getEntriesByName(name)[0];
-          console.log(measurement);
+
+          F.Events.one('events').customPOST(buildPostData(measurement));
         }
       }
     };
