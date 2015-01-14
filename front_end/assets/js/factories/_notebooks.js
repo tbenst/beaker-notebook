735814c392b366d1a788d29962052454842f3f79
Sam Saccone <samccone@gmail.com>
2015-01-14 15:18:29 -0500

    Split Notebook Factory DI to multiline

--- front_end/assets/js/factories/_notebooks.js
+++ front_end/assets/js/factories/_notebooks.js
@@ -1,43 +1,51 @@
 ;(function(angular, app) {
-  app.factory('NotebooksFactory', ["Restangular", '$window', '$location', function(Restangular, $window, $location) {
-    var R = Restangular;
+  app.factory('NotebooksFactory', [
+    "Restangular",
+    '$window',
+    '$location',
+    function(
+      Restangular,
+      $window,
+      $location) {
 
-    function project(id) {
-      return R.one('projects', id);
-    }
+      var R = Restangular;
 
-    return {
-      getNotebook: function(notebookId) {
-        return R.one('notebooks', notebookId).get();
-      },
-
-      getNotebooks: function() {
-        return R.all('notebooks').getList();
-      },
-
-      getOpened: function() {
-        return R.all('notebooks').getList({opened: true});
-      },
-
-      update: function(attrs) {
-        return R.one('notebooks', attrs.id).customPUT(attrs);
-      },
-
-      publish: function(attrs) {
-        return R.all('publications').post(attrs);
-      },
-
-      updatePublication: function(attrs) {
-        return R.one('publications', attrs.id).customPUT(attrs);
-      },
-
-      destroy: function(id) {
-        return R.one('notebooks', id).remove();
-      },
-
-      createNotebook: function(projectId, attrs) {
-        return project(projectId).all('notebooks').post(attrs);
+      function project(id) {
+        return R.one('projects', id);
       }
+
+      return {
+        getNotebook: function(notebookId) {
+          return R.one('notebooks', notebookId).get();
+        },
+
+        getNotebooks: function() {
+          return R.all('notebooks').getList();
+        },
+
+        getOpened: function() {
+          return R.all('notebooks').getList({opened: true});
+        },
+
+        update: function(attrs) {
+          return R.one('notebooks', attrs.id).customPUT(attrs);
+        },
+
+        publish: function(attrs) {
+          return R.all('publications').post(attrs);
+        },
+
+        updatePublication: function(attrs) {
+          return R.one('publications', attrs.id).customPUT(attrs);
+        },
+
+        destroy: function(id) {
+          return R.one('notebooks', id).remove();
+        },
+
+        createNotebook: function(projectId, attrs) {
+          return project(projectId).all('notebooks').post(attrs);
+        }
     };
   }]);
 })(angular, window.bunsen);
