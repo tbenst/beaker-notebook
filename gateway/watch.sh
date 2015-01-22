528519913681b04d2b19736be6802530211778a7
Matthew Forsyth <matt@mojotech.com>
2015-01-22 09:38:14 -0500

    gateway:  Remove gateway.

--- gateway/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o nounset -o pipefail
-
-while :; do
-  sleep 10
-  ./read_docker_services.sh > /etc/gateway/routes.d/docker_services.json
-  ./read_docker_beakers.sh > /etc/gateway/routes.d/docker_beakers.json
-  ./update_haproxy.sh
-done
