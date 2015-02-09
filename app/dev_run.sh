1b07f51f1b30b3786909cdacfbb95fe0cbc5bca8
Will Farrell <will@mojotech.com>
2015-02-09 09:55:06 -0500

    api: Prepare for running locally.

--- app/dev_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# Clean up running node-inspector pm2 fork processes
-kill $(lsof -i :8080 | awk 'FNR==2 {print $2}') && \
-kill $(lsof -i :5858 | awk 'FNR==2 {print $2}')
-
-node-debug --hidden=node_modules -–no-preload --debug-brk=0 --web-host=0.0.0.0 --cli app.js
