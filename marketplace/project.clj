8e0de8468fa7f477b69b144117a0d5e6ac8434a9
Matthew Forsyth <matt@mojotech.com>
2015-02-17 16:48:05 -0500

    marketplace: Reorganize project source tree.
    Move both test and main code under src/

--- marketplace/project.clj
+++ marketplace/project.clj
@@ -1,6 +1,6 @@
 (defproject indexer "0.1.0-SNAPSHOT"
   :description "Manages the Marketplace data for Bunsen."
-  :resource-paths ["resources"]
+  :url "https://github.com/twosigma/bunsen"
   :jvm-opts ["-Dsun.security.jgss.native=true"]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
@@ -8,3 +8,7 @@
                  [clojurewerkz/elastisch "2.1.0-beta6"]
                  [wkf/clj-http "1.0.1-SNAPSHOT-MOJO"]
                  [com.taoensso/timbre "3.3.0"]])
+  :source-paths ["src/main"]
+  :test-paths ["src/test"]
+  :resource-paths ["resources"]
+  :aliases {"build" ["do" ["clean"] ["uberjar"]]})
