b53b1f6014a85fe2da813df4e3e5c2b7e710baed
grin <bartek@mojotech.com>
2015-03-13 13:20:29 +0100

    publications: Enable JSON serialization of date/time values in Liberator responses

--- publications/src/main/bunsen/publications/service.clj
+++ publications/src/main/bunsen/publications/service.clj
@@ -1,11 +1,13 @@
 (ns bunsen.publications.service
   (:gen-class)
   (:require [environ.core :refer [env]]
+            [bunsen.publications.helper.json :as json]
             [com.stuartsierra.component :as component]
             [bunsen.publications.component.database :refer [database]]
             [bunsen.publications.component.server :refer [server]]))
 
 (defn service [config]
+  (json/enable-date-serialization)
   (-> (component/system-map
         :database (database config)
         :server (component/using
