52aab97754c69c43dc19061cf0278b04c5826aaa
Sam Saccone <samccone@gmail.com>
2015-03-24 17:46:21 -0400

    front_end: Add a marketplace restangular service

---
+++ front_end/assets/js/services/_marketplace_restangular_service.js
@@ -0,0 +1,11 @@
+;(function(app) {
+  app.service('MarketplaceRestangular', [
+    'Restangular',
+    function(
+      R) {
+        return R.withConfig(function(config){
+          config.setBaseUrl('api/v1');
+        });
+      }
+  ]);
+})(window.bunsen);
