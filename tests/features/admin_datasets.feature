15166fcabd472cfde4383a5ac62a8d72db5be74f
Sam Saccone <samccone@gmail.com>
2015-03-27 12:42:36 -0400

    tests: Test editing a dataset

--- tests/features/admin_datasets.feature
+++ tests/features/admin_datasets.feature
@@ -1,7 +1,16 @@
 Feature: Admin Datasets
-  Scenario: Viewing the marketplace
+  Background:
     Given I'm signed in as an administrator
     And I have a default catalog
     And there is a market item
+
+  Scenario: Viewing the marketplace
     When I view the market search
     Then I should see the edit indicator
+
+  Scenario: Editing a dataset
+    When I view the market search
+    And I edit a dataset
+    And I update the dataset name to "wow"
+    And I view the market search
+    Then I should see a dataset with the name "wow"
--- tests/step_definitions/admin_datasets.js
+++ tests/step_definitions/admin_datasets.js
@@ -3,4 +3,31 @@ module.exports = function() {
     return this.W.isPresent('.admin-action.edit')
     .should.eventually.eql(true);
   });
-};
+
+  this.When(/^I edit a dataset$/, function() {
+    return this.W.click('.admin-action.edit');
+  });
+
+  this.When(/^I update the dataset name to "([^"]*)"$/, function(newName) {
+    return this.W.fill({
+      selector: '[ng-model="dataset.title"]',
+      value: newName
+    })
+    .then(function() {
+      return this.W.click('.submit-dataset-edit');
+    }.bind(this))
+    .then(function() {
+      return this.driver.sleep(2000);
+    }.bind(this));
+  });
+
+  this.Then(/^I should see a dataset with the name "([^"]*)"$/, function(newName) {
+    return new this.Widgets.MarketList().invoke({
+      method: 'getText',
+      arguments: [
+        '.title'
+      ]
+    })
+    .should.eventually.eql([newName]);
+  })
+;};
