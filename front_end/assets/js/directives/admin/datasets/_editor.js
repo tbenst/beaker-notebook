c3ff3966d1b1738d71d39ef3ea1aaa2911c8f1aa
Sam Saccone <samccone@gmail.com>
2015-03-31 14:40:16 -0400

    style: Fix formatting

--- front_end/assets/js/directives/admin/datasets/_editor.js
+++ front_end/assets/js/directives/admin/datasets/_editor.js
@@ -8,15 +8,15 @@
       $rootScope,
       $compile) {
 
-    return {
-      restrict: 'E',
-      template: templates['directives/admin/datasets/editor'],
-      scope: {
-        dataset: '=',
-        creating: '=',
-        onEdit: '&'
-      }
-    };
-  }]);
+      return {
+        restrict: 'E',
+        template: templates['directives/admin/datasets/editor'],
+        scope: {
+          dataset: '=',
+          creating: '=',
+          onEdit: '&'
+        }
+      };
+    }
+  ]);
 })(angular, window.bunsen);
-
