0a96e09dca1b5bfba8d55dda4981188c7dd68e89
Sam Saccone <samccone@gmail.com>
2015-03-31 15:31:06 -0400

    front_end: Enable deleting of a dataset

--- front_end/assets/js/directives/admin/datasets/_editor.js
+++ front_end/assets/js/directives/admin/datasets/_editor.js
@@ -3,10 +3,12 @@
     'Notebooks',
     '$rootScope',
     '$compile',
+    'MarketplaceRestangular',
     function(
       Notebooks,
       $rootScope,
-      $compile) {
+      $compile,
+      MarketplaceRestangular) {
 
       return {
         restrict: 'E',
@@ -15,7 +17,22 @@
           dataset: '=',
           creating: '=',
           onEdit: '&'
-        }
+        },
+        controller: ['$scope', '$state', function($scope, $state) {
+          $scope.deleteEntity = function(dataset) {
+            if (!confirm('Are you sure you want to delete this dataset?')) {
+              return;
+            }
+
+            MarketplaceRestangular
+            .one('indices', dataset.index)
+            .one('datasets/' +  dataset.id)
+            .remove()
+            .then(function() {
+              $state.go('admin.index');
+            });
+          };
+        }]
       };
     }
   ]);
