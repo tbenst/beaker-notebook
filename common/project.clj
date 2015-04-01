01ed46264e8d5804d6b01a9a1d3f1a10d5ad6ae1
grin <bartek@mojotech.com>
2015-04-01 12:20:13 +0200

    common, marketplace: Factor out bunsen session store into common module

--- common/project.clj
+++ common/project.clj
@@ -3,5 +3,8 @@
   :test-paths ["src/test"]
   :source-paths ["src/main"]
   :dependencies [[org.clojure/clojure _]
+                 [org.clojure/data.codec "0.1.0"]
+                 [pandect "0.5.1"]
+                 [wkf/clj-http _]
                  [org.clojure/data.json _]]
   :plugins [[lein-modules "0.3.10"]])
