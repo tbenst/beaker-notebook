78f699be73bb24876f4d390ee3c72b1b8f9c9c46
Sam Saccone <samccone@gmail.com>
2015-04-06 15:41:57 -0400

    style: Fix indentation level (off by 1)

--- front_end/assets/js/controllers/_market_item.js
+++ front_end/assets/js/controllers/_market_item.js
@@ -18,75 +18,75 @@
       DataFormatService,
       AuthService) {
 
-    var R = Restangular;
-    var F = Factories;
-    var DFS = DataFormatService;
+      var R = Restangular;
+      var F = Factories;
+      var DFS = DataFormatService;
 
-    $scope.item = {};
+      $scope.item = {};
 
-    if (AuthService.isUserAdmin()) {
-      $scope.isAdmin = true;
-    }
-
-    var unbindWatcher = $scope.$watch('user', function(v) {
-      if (v) {
-        getItemWithRatings();
-        unbindWatcher();
+      if (AuthService.isUserAdmin()) {
+        $scope.isAdmin = true;
       }
-    });
 
-    function getItemWithRatings() {
-      $scope.ratingAttrs = {
-        rateableId: 'data_sets' + '-' + $state.params.index + '-' + $state.params.id.toString()
+      var unbindWatcher = $scope.$watch('user', function(v) {
+        if (v) {
+          getItemWithRatings();
+          unbindWatcher();
+        }
+      });
+
+      function getItemWithRatings() {
+        $scope.ratingAttrs = {
+          rateableId: 'data_sets' + '-' + $state.params.index + '-' + $state.params.id.toString()
+        };
+
+        F.DataSets.getDataSet($state.params.index, $state.params.id).then(function(d) {
+          $scope.item = Restangular.stripRestangular(d);
+          $scope.subscribed = _.contains(d.subscriberIds, $sessionStorage.user.id);
+          if ($scope.item.csvPreview) {
+            $scope.item.tabView = 'table';
+            $scope.tableDataPreview = DFS.buildTable($scope.item);
+          }
+          else if ($scope.item.dataPreviews !== undefined ) {
+            $scope.item.tabView = 'thumbnail';
+          }
+
+          F.Ratings.averageRating($scope.ratingAttrs)
+          .then(function(count) {
+            _.extend($scope.item, {averageRating: parseFloat(count)});
+          });
+
+          F.Ratings.userRating($scope.ratingAttrs)
+          .then(function(rate) {
+            _.extend($scope.item, {userRating: rate});
+          });
+        });
+      }
+
+      if (_.contains($rootScope.referrer.fromState.name, 'subscriptions')) {
+        $scope.referrerList = 'subscriptions.items';
+      } else {
+        $scope.referrerList = 'marketPlace.items';
+      }
+
+      $scope.searchByVendor = function(vendor) {
+        $scope.newSearch({vendorScope: [vendor]});
       };
 
-      F.DataSets.getDataSet($state.params.index, $state.params.id).then(function(d) {
-        $scope.item = Restangular.stripRestangular(d);
-        $scope.subscribed = _.contains(d.subscriberIds, $sessionStorage.user.id);
-        if ($scope.item.csvPreview) {
-          $scope.item.tabView = 'table';
-          $scope.tableDataPreview = DFS.buildTable($scope.item);
-        }
-        else if ($scope.item.dataPreviews !== undefined ) {
-          $scope.item.tabView = 'thumbnail';
-        }
+      function restangularSubscription() {
+        return R.all('subscriptions').one($state.params.index, $state.params.id);
+      }
 
-        F.Ratings.averageRating($scope.ratingAttrs)
-        .then(function(count) {
-          _.extend($scope.item, {averageRating: parseFloat(count)});
+      $scope.unsubscribe = function() {
+        restangularSubscription().remove().then(function(d) {
+          $scope.subscribed = false;
         });
+      };
 
-        F.Ratings.userRating($scope.ratingAttrs)
-        .then(function(rate) {
-          _.extend($scope.item, {userRating: rate});
+      $scope.subscribe = function() {
+        restangularSubscription().put().then(function(d) {
+          $scope.subscribed = true;
         });
-      });
-    }
-
-    if (_.contains($rootScope.referrer.fromState.name, 'subscriptions')) {
-      $scope.referrerList = 'subscriptions.items';
-    } else {
-      $scope.referrerList = 'marketPlace.items';
-    }
-
-    $scope.searchByVendor = function(vendor) {
-      $scope.newSearch({vendorScope: [vendor]});
-    };
-
-    function restangularSubscription() {
-      return R.all('subscriptions').one($state.params.index, $state.params.id);
-    }
-
-    $scope.unsubscribe = function() {
-      restangularSubscription().remove().then(function(d) {
-        $scope.subscribed = false;
-      });
-    };
-
-    $scope.subscribe = function() {
-      restangularSubscription().put().then(function(d) {
-        $scope.subscribed = true;
-      });
-    };
-  }]);
+      };
+    }]);
 })(angular, window.bunsen);
