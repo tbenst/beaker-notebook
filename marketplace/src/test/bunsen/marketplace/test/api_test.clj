05f9bab888694960173726f3c14c131a262b6ecb
Seth Krasnianski <seth@mojotech.com>
2015-04-06 15:53:46 -0400

    marketplace: Create tests for tags endpoint

--- marketplace/src/test/bunsen/marketplace/test/api_test.clj
+++ marketplace/src/test/bunsen/marketplace/test/api_test.clj
@@ -53,4 +53,9 @@
     (is (= 3 (count (json/read-str (:body (fetch "/marketplace/v1/formats" {:cookie-store (sign-in 1)
                                                                             :content-type :json}))))))))
 
+(deftest get-tags
+  (testing "finding all tags"
+    (is (= 14 (count (json/read-str (:body (fetch "/marketplace/v1/tags" {:cookie-store (sign-in 1)
+                                                                          :content-type :json}))))))))
+
 (use-fixtures :each setup-market-tests)
