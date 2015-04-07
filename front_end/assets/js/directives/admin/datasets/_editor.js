787903e292639a6764d40bbbd40bb3df89d6d38a
Carlos Gomes <carlos@mojotech.com>
2015-04-07 10:01:46 -0400

    web:Use typeahead for dataset category selection

--- front_end/assets/js/directives/admin/datasets/_editor.js
+++ front_end/assets/js/directives/admin/datasets/_editor.js
@@ -2,9 +2,11 @@
   app.directive('datasetEditor', [
     'Notebooks',
     'MarketplaceRestangular',
+    'Factories',
     function(
       Notebooks,
-      MarketplaceRestangular) {
+      MarketplaceRestangular,
+      Factories) {
 
       return {
         restrict: 'E',
@@ -27,6 +29,25 @@
             .then(function() {
               $state.go('admin.index');
             });
+          };
+
+          $scope.getCategories = function(searchTerm) {
+            return Factories.Categories.getMarketPlaceCategories({'index-name': $scope.dataset.index, 'search-term': searchTerm});
+          };
+
+          //This function is used for angular bootstrap typeahead
+          //The first return is a guard against trying to access category if it doesn't exist
+          //When no input is entered, It returns the current ng-model "category"
+          //When input is entered, it returns the formatted results from the search "name (path)"
+          $scope.label = function(category) {
+            if (!category) { return; }
+            if (!category.name) { return category; }
+            return category.name + ' (' + category.path + ')';
+          };
+
+          $scope.onSelect = function(category) {
+            $scope.dataset.categories[0] = _.pick(category, 'id', 'name', 'path');
+            $scope.dataset.categoryIds[0] = category.id;
           };
         }]
       };
--- front_end/assets/js/factories/_categories.js
+++ front_end/assets/js/factories/_categories.js
@@ -1,11 +1,14 @@
 !(function(app) {
 
-  app.factory('CategoriesFactory', ['Restangular', function(Restangular) {
+  app.factory('CategoriesFactory', ['Restangular', 'MarketplaceRestangular', function(Restangular, MR) {
     return {
       getCategories: function(queryParams) {
         return Restangular.all('categories').getList(queryParams);
+      },
+      getMarketPlaceCategories: function(params) {
+        return MR.all('categories').getList(params);
       }
-    }
+    };
   }]);
 
 })(window.bunsen);
