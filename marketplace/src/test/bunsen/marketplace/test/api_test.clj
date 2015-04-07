92d2a22f53c7a9d1dbfb0dfdf7bbe5eb2e60e578
Seth Krasnianski <seth@mojotech.com>
2015-04-07 13:37:48 -0400

    marketplace: Create tests for vendors endpoint

--- marketplace/src/test/bunsen/marketplace/test/api_test.clj
+++ marketplace/src/test/bunsen/marketplace/test/api_test.clj
@@ -58,4 +58,9 @@
     (is (= 11 (count (json/read-str (:body (fetch "/marketplace/v1/tags" {:cookie-store (sign-in 1)
                                                                           :content-type :json}))))))))
 
+(deftest get-vendors
+  (testing "finding all vendors"
+    (is (= 2 (count (json/read-str (:body (fetch "/marketplace/v1/vendors" {:cookie-store (sign-in 1)
+                                                                            :content-type :json}))))))))
+
 (use-fixtures :each setup-market-tests)
