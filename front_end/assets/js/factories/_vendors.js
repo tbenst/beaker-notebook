44dc7a826dd2d12768af75c0ad5a106ea613ba6f
Carlos Gomes <carlos@mojotech.com>
2015-04-08 14:17:15 -0400

    style: Fix quote style

--- front_end/assets/js/factories/_vendors.js
+++ front_end/assets/js/factories/_vendors.js
@@ -1,5 +1,5 @@
 ;(function(angular, app) {
-  app.factory('VendorsFactory', ["Restangular", function(Restangular) {
+  app.factory('VendorsFactory', ['Restangular', function(Restangular) {
     var R = Restangular;
 
     return {
