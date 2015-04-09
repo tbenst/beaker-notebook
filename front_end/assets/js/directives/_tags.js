9018bc43d1d81ee276274447ecb2331c4492edb3
Carlos Gomes <carlos@mojotech.com>
2015-04-09 12:58:22 -0400

    web: Add tags directive

---
+++ front_end/assets/js/directives/_tags.js
@@ -0,0 +1,37 @@
+;(function(angular, app) {
+  app.directive('tags', [
+    'Factories',
+    function(
+      Factories) {
+      return {
+        restrict: 'E',
+        scope: {
+          datasetTags:  '='
+        },
+        template: templates['directives/tag_display'],
+        controller: ['$scope', function($scope) {
+
+          function getTags() {
+            Factories.Tags.getTags()
+            .then(function(tags) {
+              $scope.allTags = _.xor(tags, $scope.datasetTags);
+            });
+          }
+
+          $scope.add = function() {
+            if (_.indexOf($scope.datasetTags, $scope.newTag) !== -1 || !$scope.newTag) {return;}
+            $scope.datasetTags.push($scope.newTag);
+            $scope.newTag = '';
+          };
+
+          $scope.remove = function(index) {
+            $scope.datasetTags.splice(index, 1);
+          };
+
+          $scope.$watchCollection('datasetTags', function() {
+            getTags();
+          });
+        }]
+      };
+    }]);
+})(angular, window.bunsen);
