78359eb9ef89715db845761ba00d73bebb8bfb13
Will Farrell <will@mojotech.com>
2015-04-15 16:15:18 -0400

    publications: Limit memory to 256MB.

--- publications/project.clj
+++ publications/project.clj
@@ -2,6 +2,7 @@
   :description "bunsen publications service"
   :url "https://github.com/twosigma/bunsen"
   :main bunsen.publications.service
+  :jvm-opts ["-Xmx256M"]
   :repositories {"my.datomic.com"
                  {:url "https://my.datomic.com/repo"
                   :username "will@mojotech.com"
