a754bee11930bd133232b5abc36abc4d0653b5d0
Sam Saccone <samccone@gmail.com>
2015-04-16 16:25:49 -0400

    tests: Silence fluxbox

--- tests/main.sh
+++ tests/main.sh
@@ -28,7 +28,7 @@ Options:
   Xvfb :99 -shmem -screen 0 1368x768x16 &
 
   # start window manager
-  fluxbox &
+  2>/dev/null 1>&2 fluxbox &
 
   # start vnc server
   x11vnc -shared -display :99 -bg -nopw -listen 0.0.0.0 -forever -logappend /var/log/x11vnc.log -xkb
