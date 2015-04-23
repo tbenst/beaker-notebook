36c33215f026ccbaeeb46ce2b7374e292a8c0769
grin <bartek@mojotech.com>
2015-04-23 21:02:53 +0200

    web: Add Users Rectangular service

---
+++ front_end/assets/js/services/_users_service.js
@@ -0,0 +1,10 @@
+;(function(app) {
+  app.service('UsersRestangular', [
+    'Restangular',
+    function(R) {
+        return R.withConfig(function(config){
+          config.setBaseUrl('user/v1');
+        });
+      }
+  ]);
+})(window.bunsen);
