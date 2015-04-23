68df9cdadf091970189b21f7733772d016361b62
grin <bartek@mojotech.com>
2015-04-23 18:49:03 +0200

    common: Add functions for writing and deleting bunsen sessions

--- common/src/test/bunsen/common/test/cookie.clj
+++ common/src/test/bunsen/common/test/cookie.clj
@@ -7,6 +7,10 @@
     (is (= "j:{\"id\":2,\"role\":0}"
         (cookie/unsign "s:j:{\"id\":2,\"role\":0}.1TlmSbivoucDYPk0CG1ngj2MRVeEwV8H6fou/7n1/ao" "r8T`628DaW90*?30)3qRx,2f8h?8(wG13:64K3=w00-8W7g962gM268D0lTS(Uq;^v15mY3gCj-u59k994_/@}W<"))))
 
+  (testing "encoding session data"
+    (is (= "s:j:{\"id\":2,\"role\":0}.1TlmSbivoucDYPk0CG1ngj2MRVeEwV8H6fou/7n1/ao"
+        (cookie/signed-session (sorted-map :id 2, :role 0) "r8T`628DaW90*?30)3qRx,2f8h?8(wG13:64K3=w00-8W7g962gM268D0lTS(Uq;^v15mY3gCj-u59k994_/@}W<"))))
+
   (testing "decoding an insecure cookie"
     (is (= "ssss" (cookie/unsign "ssss", "foo"))))
 
