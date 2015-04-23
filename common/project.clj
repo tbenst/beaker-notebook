4d8ba30c51837a7486b0d267d3a66e6f4b120bf9
grin <bartek@mojotech.com>
2015-04-23 18:39:41 +0200

    common: Add Datomic database component
    Has ability to migrate and seed database.

--- common/project.clj
+++ common/project.clj
@@ -2,9 +2,15 @@
   :description "Bunsen common module."
   :test-paths ["src/test"]
   :source-paths ["src/main"]
+  :repositories {"my.datomic.com"
+                 {:url "https://my.datomic.com/repo"
+                  :username "will@mojotech.com"
+                  :password "9b571a50-cb08-48ba-bd2c-86ccf58b1ad2"}}
   :dependencies [[org.clojure/clojure _]
                  [org.clojure/data.codec "0.1.0"]
                  [pandect "0.5.1"]
                  [wkf/clj-http _]
+                 [com.datomic/datomic-pro "0.9.5153"]
+                 [io.rkn/conformity "0.3.3"]
                  [org.clojure/data.json _]]
   :plugins [[lein-modules "0.3.10"]])
