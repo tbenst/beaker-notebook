ae87e38d599a7d0895e71117d3fb485d18a2c93f
grin <bartek@mojotech.com>
2015-04-23 19:25:26 +0200

    user: Enable UUID serialization in JSON responses

--- user/src/main/bunsen/user/service.clj
+++ user/src/main/bunsen/user/service.clj
@@ -4,9 +4,11 @@
             [com.stuartsierra.component :as component]
             [crypto.password.bcrypt :as password]
             [bunsen.common.component.database :refer [database]]
+            [bunsen.common.helper.json :as j]
             [bunsen.user.component.server :refer [server]]))
 
 (defn service [config]
+  (j/enable-uuid-json-serialization)
   (-> (component/system-map
         :database (database (assoc config :seed-readers {'bcrypt password/encrypt}))
         :server (component/using
