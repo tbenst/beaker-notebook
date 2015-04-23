6e50937baa0bdd884545d638d4752ee21cc63eab
grin <bartek@mojotech.com>
2015-04-23 20:46:15 +0200

    api: Remove parts of auth code that's already implemented by User service
    Only "forgot password" functionality is kept.

--- app/controllers/auth_controller.js
+++ app/controllers/auth_controller.js
@@ -6,37 +6,7 @@ module.exports = function(app) {
   var User = app.Models.User;
   var FPR  = app.Models.ForgotPasswordRequests;
 
-  function sendUser(res, user, expDate) {
-    var defaultDate = new Date();
-    defaultDate.setDate(defaultDate.getDate() + 31);
-
-    // composite session store with for the user id and the user role
-    // allowing for all of the services to access this info with the shared secret.
-    res.cookie('session', {id: user.id, role: user.get('role')}, {signed: true, expires: expDate||defaultDate});
-
-    //TODO remove the user cookie
-    res.cookie('user', user.id, {signed: true, expires: expDate||defaultDate});
-    res.cookie('beakerauth', user.get('beakerPassword'), {expires: defaultDate});
-    res.json(user);
-  }
-
   return {
-    setUserCookie: sendUser,
-    authenticate: function (req, res, next) {
-     User.signIn(req.body)
-        .then(function(user) {
-          return user.getOrCreateBeakerToken()
-          .then(function() {
-            if(user) {
-              sendUser(res, user);
-            } else {
-              res.statusCode = 403;
-            }
-          })
-        })
-        .catch(next);
-    },
-
     authorize: function(req, res, next) {
       if (app.shouldSkip(req.path, 'authorize')) {
         next();
@@ -52,32 +22,8 @@ module.exports = function(app) {
           })
           .done(next, next);
       } else {
-        res.clearCookie('user');
         res.send(403);
       }
-    },
-
-    signUp: function(req, res, next) {
-      var expDate = new Date();
-      expDate.setDate(expDate.getDate() + 31);
-
-      User.forge(_.pick(req.body, 'name', 'email', 'password'))
-      .save()
-      .then(function(user) {
-        return user.getOrCreateBeakerToken()
-        .then(function() {
-          if(user) {
-            sendUser(res, user, expDate);
-          }
-        })
-      })
-      .catch(function(err) {
-        if (err.name == "RecordNotUniqueError") {
-          return res.status(409).send(err);
-        }
-
-        next(err);
-      });
     },
 
     forgotPassword: function(req, res, next) {
@@ -106,11 +52,6 @@ module.exports = function(app) {
             res.status(500).send(err.message);
           }
         });
-    },
-
-    signOut: function(req, res, next) {
-      res.clearCookie('user');
-      res.status(200).end();
     }
   }
 }
--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -1,9 +1,6 @@
 module.exports = function(app) {
   var AuthController = app.Controllers.AuthController;
 
-  app.post('/api/session', AuthController.authenticate);
-  app.del('/api/session', AuthController.signOut);
-  app.post('/api/sign_up', AuthController.signUp);
   app.post('/api/forgot_password', AuthController.forgotPassword);
   app.post('/api/change_password', AuthController.changePassword);
 }
