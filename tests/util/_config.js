d4d765dff8806eb296e7803c3857539b2e01b204
grin <bartek@mojotech.com>
2015-04-28 22:46:58 +0200

    tests: Expose User service to test helpers

--- tests/util/_config.js
+++ tests/util/_config.js
@@ -3,5 +3,6 @@ module.exports = {
   marketplaceUrl: 'http://' + (process.env.MARKETPLACE_HOST || 'localhost') +
     ':' + (process.env.MARKETPLACE_PORT || '8444') + '/marketplace/v1',
   publicationsUrl: 'http://' + (process.env.PUBLICATIONS_HOST || 'localhost') +
-    ':9000/publications/v1'
+    ':9000/publications/v1',
+  userUrl: 'http://' + (process.env.USER_HOST || 'localhost') + ':9000/user/v1'
 };
