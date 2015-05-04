e6284482285a1639963750a1d5c954ee09135fb3
Davide Pasetto <davide_pasetto@twosigma.com>
2015-05-04 14:24:47 +0000

    Implemented (optional) SSL termination in marketplace service

--- marketplace/src/main/bunsen/marketplace/component/config.clj
+++ marketplace/src/main/bunsen/marketplace/component/config.clj
@@ -4,8 +4,7 @@
   ":server-port e.g. 8444
    :elasticsearch-url, e.g. http://10.10.10.10:9200"
   [env]
-  {:server-port (Integer. (:marketplace-port env))
-   :cookie-salt (:cookie-salt env)
+  {:cookie-salt (:cookie-salt env)
    :allow-seed (:allow-seed env)
    :elasticsearch-url (format "http://%s:%s"
                               (:elasticsearch-host env)
@@ -14,4 +13,17 @@
                                 pass (:elasticsearch-pass env)]
                             (if-not (and user pass)
                               {}
-                              {:basic-auth [user pass]}))})
+                              {:basic-auth [user pass]}))
+   :jetty-options (let [keystore (:ssl-keystore env)
+   		      keystore-pass (:ssl-keystore-pass env)]
+                  (if-not (and keystore keystore-pass)
+		      {:port (Integer. (:marketplace-port env))
+		       :ssl? false
+		       :join? false}
+		      {:ssl? true
+		       :port (+ (Integer. (:marketplace-port env)) 1)
+		       :ssl-port (Integer. (:marketplace-port env))
+		       :keystore (:ssl-keystore env)
+		       :key-password (:ssl-keystore-pass env)
+		       :join? false}))})
+
