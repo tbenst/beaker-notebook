5cab4dc0d15d5853ecc36538e5a7d12067d50b6e
Will Farrell <will@mojotech.com>
2015-05-07 08:45:21 -0400

    user: Excise users instead of recreating database.

--- user/src/main/bunsen/user/resource/seed.clj
+++ user/src/main/bunsen/user/resource/seed.clj
@@ -1,6 +1,7 @@
 (ns bunsen.user.resource.seed
   (:require [liberator.core :refer [defresource]]
             [datomic.api :as d]
+            [bunsen.user.model.user :as u]
             [bunsen.common.component.database :as db]
             [bunsen.user.resource.defaults :refer [defaults]]))
 
@@ -10,8 +11,5 @@
   :allowed-methods [:delete]
 
   :delete! (fn [ctx]
-             (let [uri (:database-uri config)
-                   conn (get-in ctx [:request :conn])]
-               (d/delete-database uri)
-               (d/create-database uri)
-               (db/migrate conn "migrations.edn"))))
+             (let [conn (-> ctx :request :conn)]
+               (u/excise-all-users! conn))))
