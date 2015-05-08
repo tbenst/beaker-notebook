4d0f35a40f66c406e0e5677fa40ea258200a1cd0
Seth Krasnianski <seth@mojotech.com>
2015-05-08 17:00:45 -0400

    style, front_end: Whitespace

--- front_end/assets/js/controllers/_help.js
+++ front_end/assets/js/controllers/_help.js
@@ -9,7 +9,7 @@
       $scope.helpState = $scope.helpState || {};
 
       $scope.topics = [
-        { title: 'Example and Styles',
+        {title: 'Example and Styles',
           state: 'help.main.topic1',
           sections: [
             {
@@ -27,26 +27,26 @@
       function currentTopic() {
         var currentStateArr = $state.current.name.split('.');
         if (currentStateArr.length < 3) return;
-        var currentTopicState = currentStateArr.splice(0,3).join('.');
-        $scope.helpState.currentTopic = _.findWhere($scope.topics, { state: currentTopicState });
+        var currentTopicState = currentStateArr.splice(0, 3).join('.');
+        $scope.helpState.currentTopic = _.findWhere($scope.topics, {state: currentTopicState});
       }
 
       currentTopic();
 
       $scope.changeSection = function(section) {
-        if(section === undefined) return;
+        if (section === undefined) return;
         $scope.helpState.currentSection = section;
       }
 
-      $scope.nextSection = function () {
+      $scope.nextSection = function() {
         var sections = $scope.helpState.currentTopic.sections;
-        var currentSectionIndex = _.findIndex(sections, { title: $scope.helpState.currentSection.title });
-        if (currentSectionIndex < sections.length - 1 ) return sections[currentSectionIndex + 1];
+        var currentSectionIndex = _.findIndex(sections, {title: $scope.helpState.currentSection.title});
+        if (currentSectionIndex < sections.length - 1) return sections[currentSectionIndex + 1];
       }
 
-      $scope.previousSection = function () {
+      $scope.previousSection = function() {
         var sections = $scope.helpState.currentTopic.sections;
-        var currentSectionIndex = _.findIndex(sections, { title: $scope.helpState.currentSection.title });
+        var currentSectionIndex = _.findIndex(sections, {title: $scope.helpState.currentSection.title});
         if (currentSectionIndex >= 1) return sections[currentSectionIndex - 1];
       }
 
