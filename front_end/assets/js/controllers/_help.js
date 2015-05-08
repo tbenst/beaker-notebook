19638ba63013d0fee2038a3644de272f68fb079a
Seth Krasnianski <seth@mojotech.com>
2015-05-08 17:02:02 -0400

    style, front_end: if statements require brackets

--- front_end/assets/js/controllers/_help.js
+++ front_end/assets/js/controllers/_help.js
@@ -26,7 +26,7 @@
 
       function currentTopic() {
         var currentStateArr = $state.current.name.split('.');
-        if (currentStateArr.length < 3) return;
+        if (currentStateArr.length < 3) { return; }
         var currentTopicState = currentStateArr.splice(0, 3).join('.');
         $scope.helpState.currentTopic = _.findWhere($scope.topics, {state: currentTopicState});
       }
@@ -34,25 +34,25 @@
       currentTopic();
 
       $scope.changeSection = function(section) {
-        if (section === undefined) return;
+        if (section === undefined) { return; }
         $scope.helpState.currentSection = section;
       }
 
       $scope.nextSection = function() {
         var sections = $scope.helpState.currentTopic.sections;
         var currentSectionIndex = _.findIndex(sections, {title: $scope.helpState.currentSection.title});
-        if (currentSectionIndex < sections.length - 1) return sections[currentSectionIndex + 1];
+        if (currentSectionIndex < sections.length - 1) { return sections[currentSectionIndex + 1]; }
       }
 
       $scope.previousSection = function() {
         var sections = $scope.helpState.currentTopic.sections;
         var currentSectionIndex = _.findIndex(sections, {title: $scope.helpState.currentSection.title});
-        if (currentSectionIndex >= 1) return sections[currentSectionIndex - 1];
+        if (currentSectionIndex >= 1) { return sections[currentSectionIndex - 1]; }
       }
 
       $scope.$watch('$state.current', function() {
         currentTopic();
-        if (!$scope.helpState.currentTopic) return;
+        if (!$scope.helpState.currentTopic) { return; }
         $scope.helpState.currentSection = $scope.helpState.currentTopic.sections[0];
       })
   }]);
