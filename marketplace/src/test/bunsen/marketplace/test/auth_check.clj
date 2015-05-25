197c1af634a89e1c5070b6b2eefa9130c9d078eb
Will Farrell <will@mojotech.com>
2015-05-25 13:40:24 -0400

    marketplace: Update auth check tests to reflect updated predicates.

--- marketplace/src/test/bunsen/marketplace/test/auth_check.clj
+++ marketplace/src/test/bunsen/marketplace/test/auth_check.clj
@@ -1,19 +1,20 @@
 (ns bunsen.marketplace.test.auth-check
-  (:require [bunsen.marketplace.helper.resource :as resource]
+  (:require [bunsen.marketplace.resource :as resource]
             [clojure.test :refer :all]))
 
 (deftest auth-check
-  (testing "auth is bypassed when seed is enabled" (is (= (resource/admin?
-                                                            {:allow-seed "true"}
-                                                            {:request {:session {:role 0}}})
-                                                          true)))
+  (testing "auth is bypassed when seed is enabled"
+    (is (resource/allow-seed?
+          {:request {:config {:allow-seed "true"}}})))
 
-  (testing "auth fails when a user does not have the correct role" (is (= (resource/admin?
-                                                                            {:allow-seed "false"}
-                                                                            {:request {:session {:role 0}}})
-                                                                          false)))
+  (testing "auth is not bypassed when seed is disabled"
+    (is (not (resource/allow-seed?
+               {:request {:config {:allow-seed "false"}}}))))
 
-  (testing "checking if a user has an admin role" (is (= (resource/admin?
-                                                           {:allow-seed "false"}
-                                                           {:request {:session {:role 1}}})
-                                                         true))))
+  (testing "checking if a user has an admin role"
+    (is (resource/admin?
+          {:request {:session {:role 1}}})))
+
+  (testing "auth fails when a user does not have the correct role"
+    (is (not (resource/admin?
+               {:request {:session {:role 0}}})))))
