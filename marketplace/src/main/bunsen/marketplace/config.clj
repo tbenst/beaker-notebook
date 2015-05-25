864269be0401fd1ccc06f32868b3f72ba4e3e13d
Will Farrell <will@mojotech.com>
2015-05-25 13:32:48 -0400

    marketplace: Stop treating config like a component.

---
+++ marketplace/src/main/bunsen/marketplace/config.clj
@@ -0,0 +1,33 @@
+(ns bunsen.marketplace.config)
+
+(defn config
+  ":server-port e.g. 8444
+   :elasticsearch-url, e.g. http://10.10.10.10:9200"
+  [env]
+  {:cookie-salt (:cookie-salt env)
+   :allow-seed (:allow-seed env)
+   :database-uri (:marketplace-database-uri env)
+   :elasticsearch-url (format "http://%s:%s"
+                              (:elasticsearch-host env)
+                              (:elasticsearch-port env))
+   :elasticsearch-options (let [user (:elasticsearch-user env)
+                                pass (:elasticsearch-pass env)]
+                            (if-not (and user pass)
+                              {}
+                              {:basic-auth [user pass]}))
+   :use-kerberos (let [kerberosprincipal (:kerberos-principal env)]
+                   (if kerberosprincipal
+                     true false))
+   :kerberos-principal (:kerberos-principal env)
+   :jetty-options (let [keystore (:ssl-keystore env)
+                        keystore-pass (:ssl-keystore-pass env)]
+                    (if-not (and keystore keystore-pass)
+                      {:port (Integer. (:marketplace-port env))
+                       :ssl? false
+                       :join? false}
+                      {:ssl? true
+                       :port (+ (Integer. (:marketplace-port env)) 1)
+                       :ssl-port (Integer. (:marketplace-port env))
+                       :keystore (:ssl-keystore env)
+                       :key-password (:ssl-keystore-pass env)
+                       :join? false}))})
