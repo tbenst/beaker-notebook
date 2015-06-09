68da90da1654ea67e980ebf38d5d9df825323d1d
Jake Buob <jake@mojotech.com>
2015-06-09 17:21:27 -0400

    tests: Remove unused beaker iframe widgets

--- tests/widgets/beaker/bunsen_button.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  this.Widgets.Beaker = this.Widgets.Beaker || {};
-
-  return this.Widgets.Beaker.BunsenButton = this.Widget.extend({
-    root: '#drop2'
-  });
-};
--- tests/widgets/beaker/insert_cell.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-  this.Widgets.Beaker = this.Widgets.Beaker || {};
-
-  return this.Widgets.Beaker.InsertCell = this.Widget.extend({
-    root: '.insert-cell-indicator'
-  });
-};
