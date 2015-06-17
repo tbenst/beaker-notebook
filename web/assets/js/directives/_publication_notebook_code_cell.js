5bd2de82e3c8954b6391a7aaa9d3c6dd694aa3e5
grin <bartek@mojotech.com>
2015-06-17 22:46:14 +0200

    web: Collapse code cells in publications

--- web/assets/js/directives/_publication_notebook_code_cell.js
+++ web/assets/js/directives/_publication_notebook_code_cell.js
@@ -1,5 +1,6 @@
 ;(function(angular, app) {
   app.controller('publication-cell-code', function($scope) {
+    $scope.cell.input.collapsed = true;
     $scope.toggleCollapsed = function(zone) {
       $scope.cell[zone] = $scope.cell[zone] || {};
       $scope.cell[zone].collapsed = !$scope.cell[zone].collapsed
