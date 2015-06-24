bfef661be5290adbae94ac1771253e929d151d82
grin <bartek@mojotech.com>
2015-06-24 14:34:26 +0200

    web: Render LaTeX in markdown and section publication cells

--- web/assets/js/directives/_publication_notebook_cells.js
+++ web/assets/js/directives/_publication_notebook_cells.js
@@ -1,4 +1,23 @@
 ;(function(angular, app) {
+  app.controller('publication-cell-latex-markdown', function($scope, $element, $attrs) {
+    var contentAttribute = $scope.cell.type === 'section' ? 'title' : 'body';
+    var body = $scope.cell[contentAttribute];
+    if (_.isArray(body)) {
+      body = body.join("\n");
+    }
+    var markdownFragment = $('<div>' + body + '</div>');
+    renderMathInElement(markdownFragment[0], {
+      delimiters: [
+        {left: "$$", right: "$$", display: true},
+        {left: "$", right:  "$", display: false},
+        {left: "\\[", right: "\\]", display: true},
+        {left: "\\(", right: "\\)", display: false}
+      ]
+    });
+    $element.html(marked(markdownFragment.html(), {gfm: true}));
+    markdownFragment.remove();
+  });
+
   app.directive('publicationCell', ['$compile', function($compile) {
     return {
       restrict: "E",
