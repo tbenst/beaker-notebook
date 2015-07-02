d611eae54f3c88c6306e3f46435c3dbec1256f37
Seth Krasnianski <seth@mojotech.com>
2015-07-02 00:57:27 -0400

    web: Fix dataset validation

--- web/assets/js/services/_dataset_helper.js
+++ web/assets/js/services/_dataset_helper.js
@@ -2,17 +2,7 @@
   app.service('DatasetHelper', function() {
     return {
       validateDataset: function(dataset) {
-        return (_.has(dataset, 'vendor') && dataset.categories !== undefined && this.validateCategories(dataset.categories));
-      },
-
-      validateCategories: function(categories) {
-        var valid = true;
-        categories.forEach(function(cat) {
-          if (cat.name === undefined) {
-            valid = false;
-          }
-        });
-        return valid;
+        return (_.has(dataset, 'vendor') && _.has(dataset, 'category'));
       },
 
       generateError: function() {
